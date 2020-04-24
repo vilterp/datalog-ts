653d6702660180307fc58a1234c2542ee4d7130a
Pete Vilter <pete.vilter@gmail.com>
2020-04-23 23:15:00 -0400

    allow specifying a query that will run
    and don't print stuff out if stdin is not a tty

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 import { Repl } from "./repl";
 
-const repl = new Repl(process.stdin, process.stdout);
+const repl = new Repl(
+  process.stdin,
+  process.stdout,
+  process.stdin.isTTY,
+  process.argv[2] || ""
+);
 repl.run();
--- repl.ts
+++ repl.ts
@@ -6,53 +6,76 @@ import { planQuery } from "./plan";
 import { allResults, instantiate } from "./execNodes";
 import { prettyPrintDB, prettyPrintPlan, prettyPrintResults } from "./pretty";
 import * as pp from "prettier-printer";
+import { ReadLine } from "readline";
 
 export class Repl {
   db: DB;
   in: NodeJS.ReadableStream;
   out: NodeJS.WritableStream;
   buffer: string;
+  stdinTTY: boolean;
+  query: string;
+  rl: readline.Interface;
 
-  constructor(input: NodeJS.ReadableStream, out: NodeJS.WritableStream) {
+  constructor(
+    input: NodeJS.ReadableStream,
+    out: NodeJS.WritableStream,
+    stdinTTY: boolean,
+    query: string
+  ) {
     this.db = newDB();
     this.in = input;
     this.out = out;
     this.buffer = "";
+    this.stdinTTY = stdinTTY;
+    this.query = query.endsWith(".") ? query : `${query}.`;
   }
 
   run() {
-    const opts: readline.ReadLineOptions = {
-      input: this.in,
-      output: this.out,
-      prompt: "> ",
-    };
+    const opts: readline.ReadLineOptions = this.stdinTTY
+      ? {
+          input: this.in,
+          output: this.out,
+          prompt: "> ",
+        }
+      : { input: this.in };
     const rl = readline.createInterface(opts);
     rl.on("line", (line) => {
-      if (line.length === 0) {
-        rl.prompt();
-        return;
-      }
-      // special commands
-      // TODO: parse these with parser
-      if (line === ".dump") {
-        console.log(pp.render(100, prettyPrintDB(this.db)));
-        rl.prompt();
-        return;
-      }
-      this.buffer = this.buffer + line;
-      if (!line.endsWith(".")) {
-        return;
-      }
-      try {
-        const stmt: Statement = language.statement.tryParse(this.buffer);
-        this.handleStmt(stmt);
-      } catch (e) {
-        console.error("parse error", e.toString());
-      } finally {
-        this.buffer = "";
-      }
-      rl.prompt();
+      this.handleLine(line);
     });
+    rl.prompt();
+
+    rl.on("close", () => {
+      this.handleLine(this.query);
+    });
+    this.rl = rl;
+  }
+
+  private handleLine(line: string) {
+    const rl = this.rl;
+    if (line.length === 0) {
+      rl.prompt();
+      return;
+    }
+    // special commands
+    // TODO: parse these with parser
+    if (line === ".dump") {
+      console.log(pp.render(100, prettyPrintDB(this.db)));
+      rl.prompt();
+      return;
+    }
+    this.buffer = this.buffer + line;
+    if (!line.endsWith(".")) {
+      return;
+    }
+    try {
+      const stmt: Statement = language.statement.tryParse(this.buffer);
+      this.handleStmt(stmt);
+    } catch (e) {
+      console.error("parse error", e.toString());
+    } finally {
+      this.buffer = "";
+    }
     rl.prompt();
   }
 
