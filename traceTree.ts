b252cbac785aafa4f35f7d1d531705bd0344e721
Pete Vilter <pete.vilter@gmail.com>
2020-06-08 01:22:49 -0400

    show bindings in trace tree nodes

--- traceTree.ts
+++ traceTree.ts
@@ -1,6 +1,6 @@
 import { Tree, leaf, node, prettyPrintTree } from "./treePrinter";
 import { Res } from "./types";
-import { ppt } from "./pretty";
+import { ppt, ppb } from "./pretty";
 
 export function prettyPrintTrace(res: Res): string {
   return prettyPrintTree(traceToTree(res));
@@ -10,11 +10,16 @@ export function traceToTree(res: Res): Tree {
   const resStr = ppt(res.term);
   switch (res.trace.type) {
     case "AndTrace":
-      return node(`And`, res.trace.sources.map(traceToTree));
+      return node(
+        `And ${ppb(res.bindings)}`,
+        res.trace.sources.map(traceToTree)
+      );
     case "MatchTrace":
-      return leaf(`Fact: ${resStr}`);
+      return leaf(`Fact: ${resStr}; ${ppb(res.bindings)}`);
     case "RefTrace":
-      return node(`Rule: ${resStr}`, [traceToTree(res.trace.innerRes)]);
+      return node(`Rule: ${resStr}; ${ppb(res.bindings)}`, [
+        traceToTree(res.trace.innerRes),
+      ]);
     case "VarTrace":
       return leaf(`var: ${resStr}`);
     case "BinExprTrace":
