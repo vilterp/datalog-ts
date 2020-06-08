d33ed4823f588d9d687745961a457ad6de50eac9
Pete Vilter <pete.vilter@gmail.com>
2020-06-07 22:11:36 -0400

    move TabbedTables to uiCommon

--- fp/ui/main.tsx
+++ fp/ui/main.tsx
@@ -13,8 +13,7 @@ import stdlibDL from "../stdlib.dl";
 import { ReplCore } from "../../replCore";
 import useLocalStorage from "react-use-localstorage";
 import { useBoolLocalStorage } from "./util";
-import * as styles from "./styles";
-import { ppt } from "../../simpleEvaluate";
+import { TabbedTables } from "../../uiCommon/tabbedTables";
 import ReactJson from "react-json-view";
 
 const loader: Loader = (path: string) => {
@@ -83,7 +82,7 @@ function Main() {
         ) : null}
       </div>
 
-      <Tabs repl={repl} />
+      <TabbedTables repl={repl} />
 
       <Collapsible
         heading="AST"
@@ -107,148 +106,6 @@ function Main() {
       <Collapsible heading="Rules" content={<pre>{typecheckDL}</pre>} />
     </div>
   );
-}
-
-function Tabs(props: { repl: ReplCore }) {
-  const allRules: Relation[] = Object.keys(props.repl.db.rules)
-    .sort()
-    .map((name) => ({ type: "Rule", name, rule: props.repl.db.rules[name] }));
-  const allTables: Relation[] = Object.keys(props.repl.db.tables)
-    .sort()
-    .map((name) => ({
-      type: "Table",
-      name,
-      records: props.repl.db.tables[name],
-    }));
-  const allRelations: Relation[] = [...allTables, ...allRules];
-  const [curRelation, setCurRelation]: [
-    string,
-    (v: string) => void
-  ] = useLocalStorage("selected-relation", allRelations[0].name);
-
-  return (
-    <div style={{ display: "flex" }}>
-      <div
-        style={{
-          padding: 15,
-          border: "1px solid black",
-          width: 150,
-        }}
-      >
-        <h4 style={{ marginBottom: 5, marginTop: 5 }}>Tables</h4>
-        {relList(allTables, curRelation, setCurRelation)}
-        <h4 style={{ marginBottom: 5, marginTop: 5 }}>Rules</h4>
-        {relList(allRules, curRelation, setCurRelation)}
-      </div>
-      <div style={{ padding: 10, border: "1px solid black", flexGrow: 1 }}>
-        <RelationTable
-          relation={allRelations.find((r) => r.name === curRelation)}
-          repl={props.repl}
-        />
-      </div>
-    </div>
-  );
-}
-
-function relList(
-  relations: Relation[],
-  curRelation: string,
-  setCurRelation: (s: string) => void
-) {
-  return (
-    <ul style={{ marginTop: 0, marginBottom: 0, paddingLeft: 20 }}>
-      {relations.map((rel) => (
-        <li
-          key={rel.name}
-          style={styles.tab(rel.name === curRelation)}
-          onClick={() => setCurRelation(rel.name)}
-        >
-          {rel.name}
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-type Relation =
-  | { type: "Table"; name: string; records: Rec[] }
-  | { type: "Rule"; name: string; rule: Rule };
-
-function RelationTable(props: { relation: Relation; repl: ReplCore }) {
-  const results: Res[] =
-    props.relation.type === "Table"
-      ? props.relation.records.map((term) => ({
-          term,
-          bindings: {},
-          trace: { type: "BaseFactTrace", fact: term },
-        }))
-      : props.repl.evalStmt({
-          type: "Insert",
-          record: props.relation.rule.head,
-        });
-  console.log({ results });
-  const records = results.map((r) => r.term);
-  const fields =
-    records.length === 0
-      ? []
-      : (props.relation.type === "Rule"
-          ? Object.keys(props.relation.rule.head.attrs)
-          : Object.keys((props.relation.records[0] as Rec).attrs)
-        ).sort((a, b) => fieldComparator(a).localeCompare(fieldComparator(b)));
-  return (
-    <>
-      {props.relation.type === "Rule" ? (
-        <pre style={{ marginTop: 5 }}>
-          {pp.render(100, prettyPrintRule(props.relation.rule))}
-        </pre>
-      ) : null}
-      {records.length === 0 ? (
-        <div style={{ fontStyle: "italic" }}>No results</div>
-      ) : (
-        <table style={{ borderCollapse: "collapse" }}>
-          <thead>
-            <tr style={{ borderBottom: "1px solid black" }}>
-              {fields.map((name) => (
-                <th key={name} style={{ paddingLeft: 5, paddingRight: 5 }}>
-                  <code>{name}</code>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {records.map((record) => (
-              <tr key={ppt(record)}>
-                {fields.map((field) => (
-                  <td
-                    key={field}
-                    style={{
-                      paddingLeft: 5,
-                      paddingRight: 5,
-                      borderLeft: "1px solid lightgrey",
-                      borderRight: "1px solid lightgrey",
-                    }}
-                  >
-                    <code>{ppt((record as Rec).attrs[field])}</code>
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
-  );
-}
-
-function fieldComparator(field: string): string {
-  switch (field) {
-    case "id":
-      return "aaaaaa_id";
-    case "location":
-      return "zzzzzz_location";
-    default:
-      return field;
-  }
 }
 
 function Collapsible(props: { heading: string; content: React.ReactNode }) {
