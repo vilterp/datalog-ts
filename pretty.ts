f29af11a45668c5ac2ba7ce51d823f44efd72968
Pete Vilter <pete.vilter@gmail.com>
2020-06-10 02:31:56 -0400

    reorder some stuff

--- pretty.ts
+++ pretty.ts
@@ -43,6 +43,81 @@ export function prettyPrintTerm(term: Term): pp.IDoc {
   }
 }
 
+export function prettyPrintRule(rule: Rule): pp.IDoc {
+  const oneLine = pp.intersperse(" | ")(
+    rule.defn.opts.map((ae) =>
+      pp.intersperse(" & ")(ae.clauses.map(prettyPrintTerm))
+    )
+  );
+  const splitUp = [
+    pp.line,
+    pp.indent(
+      2,
+      pp.intersperse([" |", pp.line])(
+        rule.defn.opts.map((ae) =>
+          pp.intersperse([" &", pp.line])(ae.clauses.map(prettyPrintTerm))
+        )
+      )
+    ),
+  ];
+  return [prettyPrintTerm(rule.head), " :- ", pp.choice(oneLine, splitUp)];
+}
+
+export function prettyPrintDB(db: DB): pp.IDoc {
+  return pp.intersperse(pp.lineBreak)(
+    [
+      ...flatMapObjToList(db.tables, (name, tbl) => tbl.map(prettyPrintTerm)),
+      ...mapObjToList(db.rules, (name, rule) => prettyPrintRule(rule)),
+    ].map((d) => [d, "."])
+  );
+}
+
+export function prettyPrintBindings(bindings: Bindings): pp.IDoc {
+  return block(
+    pp.braces,
+    mapObjToList(bindings, (key, val) => [key, ": ", prettyPrintTerm(val)])
+  );
+}
+
+export function prettyPrintRes(res: Res): pp.IDoc {
+  return [prettyPrintTerm(res.term), "; ", prettyPrintBindings(res.bindings)];
+}
+
+export function prettyPrintResults(results: Res[]): pp.IDoc {
+  return pp.intersperse(pp.line)(results.map(prettyPrintRes));
+}
+
+// compact convenience functions, straight to string
+
+export function ppt(t: Term): string {
+  return pp.render(100, prettyPrintTerm(t));
+}
+
+export function ppb(b: Bindings): string {
+  return pp.render(100, prettyPrintBindings(b));
+}
+
+export function ppr(r: Res): string {
+  return pp.render(100, prettyPrintRes(r));
+}
+
+export function ppVM(vm: VarMappings): string {
+  return pp.render(100, prettyPrintVarMappings(vm));
+}
+
+function prettyPrintVarMappings(vm: VarMappings): pp.IDoc {
+  return [
+    "{",
+    pp.intersperse(
+      ", ",
+      mapObjToList(vm, (key, value) => [key, ": ", value])
+    ),
+    "}",
+  ];
+}
+
+// trace stuff
+
 export function prettyPrintTermWithBindings(
   term: TermWithBindings,
   scopePath: ScopePath,
@@ -121,95 +196,6 @@ function prettyPrintTraceNode(
   }
 }
 
-export function prettyPrintRule(rule: Rule): pp.IDoc {
-  const oneLine = pp.intersperse(" | ")(
-    rule.defn.opts.map((ae) =>
-      pp.intersperse(" & ")(ae.clauses.map(prettyPrintTerm))
-    )
-  );
-  const splitUp = [
-    pp.line,
-    pp.indent(
-      2,
-      pp.intersperse([" |", pp.line])(
-        rule.defn.opts.map((ae) =>
-          pp.intersperse([" &", pp.line])(ae.clauses.map(prettyPrintTerm))
-        )
-      )
-    ),
-  ];
-  return [prettyPrintTerm(rule.head), " :- ", pp.choice(oneLine, splitUp)];
-}
-
-export function prettyPrintDB(db: DB): pp.IDoc {
-  return pp.intersperse(pp.lineBreak)(
-    [
-      ...flatMapObjToList(db.tables, (name, tbl) => tbl.map(prettyPrintTerm)),
-      ...mapObjToList(db.rules, (name, rule) => prettyPrintRule(rule)),
-    ].map((d) => [d, "."])
-  );
-}
-
-export function prettyPrintBindings(bindings: Bindings): pp.IDoc {
-  return block(
-    pp.braces,
-    mapObjToList(bindings, (key, val) => [key, ": ", prettyPrintTerm(val)])
-  );
-}
-
-export function prettyPrintRes(res: Res): pp.IDoc {
-  return [prettyPrintTerm(res.term), "; ", prettyPrintBindings(res.bindings)];
-}
-
-export function prettyPrintResults(results: Res[]): pp.IDoc {
-  return pp.intersperse(pp.line)(results.map(prettyPrintRes));
-}
-
-// compact convenience functions, straight to string
-
-export function ppt(t: Term): string {
-  return pp.render(100, prettyPrintTerm(t));
-}
-
-export function ppb(b: Bindings): string {
-  return pp.render(100, prettyPrintBindings(b));
-}
-
-export function ppr(r: Res): string {
-  return pp.render(100, prettyPrintRes(r));
-}
-
-export function ppVM(vm: VarMappings): string {
-  return pp.render(100, prettyPrintVarMappings(vm));
-}
-
-function prettyPrintVarMappings(vm: VarMappings): pp.IDoc {
-  return [
-    "{",
-    pp.intersperse(
-      ", ",
-      mapObjToList(vm, (key, value) => [key, ": ", value])
-    ),
-    "}",
-  ];
-}
-
-// util
-
-export function block(pair: [pp.IDoc, pp.IDoc], docs: pp.IDoc[]): pp.IDoc {
-  return [pair[0], pp.intersperse(", ")(docs), pair[1]];
-}
-
-export function escapeString(str: string): string {
-  return str
-    .split("\\")
-    .join("\\\\")
-    .split(`"`)
-    .join(`\\"`)
-    .split("\n")
-    .join("\\n");
-}
-
 export function prettyPrintScopePath(path: ScopePath): pp.IDoc {
   return [
     "[",
@@ -237,4 +223,20 @@ export function prettyPrintInvokeLoc(il: InvocationLocation): pp.IDoc {
     ),
     "]",
   ];
+}
+
+// util
+
+export function block(pair: [pp.IDoc, pp.IDoc], docs: pp.IDoc[]): pp.IDoc {
+  return [pair[0], pp.intersperse(", ")(docs), pair[1]];
+}
+
+export function escapeString(str: string): string {
+  return str
+    .split("\\")
+    .join("\\\\")
+    .split(`"`)
+    .join(`\\"`)
+    .split("\n")
+    .join("\\n");
 }
