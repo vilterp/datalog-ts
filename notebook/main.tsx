a5002663fca66068daecfa607f412b5dc4d87887
Pete Vilter <pete.vilter@gmail.com>
2020-07-03 16:58:59 -0400

    load stuff with use fetch

--- notebook/main.tsx
+++ notebook/main.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useFetch } from "use-http";
 
 function Main() {
-  return <p>Notebook viewer</p>;
+  const gistURL =
+    "https://gist.githubusercontent.com/vilterp/5b506496968c8379717888b04f35ebbf/raw/1ffa006ba9fb4b7955e051f4a5d91bdbd37c04bc/hm-with-ids-test.prolog";
+
+  const { loading, error, data = "" } = useFetch(gistURL, {}, []);
+
+  return (
+    <>
+      <p>Notebook viewer</p>
+      <pre>{data}</pre>
+    </>
+  );
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"));
