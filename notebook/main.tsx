a5af5bbb77fb5758755cc53550328cf459c48c19
Pete Vilter <pete.vilter@gmail.com>
2020-07-03 17:17:40 -0400

    routing

--- notebook/main.tsx
+++ notebook/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useFetch } from "use-http";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function Main() {
-  const gistURL =
-    "https://gist.githubusercontent.com/vilterp/5b506496968c8379717888b04f35ebbf/raw/1ffa006ba9fb4b7955e051f4a5d91bdbd37c04bc/hm-with-ids-test.prolog";
+function Viewer(props: {
+  username: string;
+  gistID: string;
+  fileID: string;
+  filename: string;
+}) {
+  const gistURL = `https://gist.githubusercontent.com/${props.username}/${props.gistID}/raw/${props.fileID}/${props.filename}`;
 
   const { loading, error, data = "" } = useFetch(gistURL, {}, []);
 
@@ -13,6 +18,46 @@ function Main() {
       <p>Notebook viewer</p>
       <pre>{data}</pre>
     </>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <h1>Notebook viewer</h1>
+      <p>Welcome</p>
+      <p>Examples:</p>
+      <ul>
+        <li>
+          <Link to="/notebook/gist/vilterp/9f06dbef549ab0fec87d7a79df05cf50/raw/ca8e208fbfcea1dc85ac3690864f0e38a4bbcdf8/family.dl">
+            Family
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route
+          path="/notebook/gist/:username/:gistID/raw/:fileID/:filename"
+          render={({ match }) => (
+            <Viewer
+              username={match.params.username}
+              gistID={match.params.gistID}
+              fileID={match.params.fileID}
+              filename={match.params.filename}
+            />
+          )}
+        />
+      </Switch>
+    </Router>
   );
 }
 
