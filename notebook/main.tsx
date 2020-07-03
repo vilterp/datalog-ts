bb1794eac6161056f7ba28d633bde53ce4e8c682
Pete Vilter <pete.vilter@gmail.com>
2020-07-03 16:48:34 -0400

    start notebook viewer

---
+++ notebook/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Main() {
+  return <p>Notebook viewer</p>;
+}
+
+ReactDOM.render(<Main />, document.getElementById("main"));
