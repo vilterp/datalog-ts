e51150cb331e39ab2766876d4396d789fcd7fd2a
Pete Vilter <pete.vilter@gmail.com>
2020-07-14 10:34:51 -0400

    initial parserlib playground

---
+++ parserlib/playground/main.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { parse } from "../parser";
+import { Grammar } from "../grammar";
+import ReactJson from "react-json-view";
+import useLocalStorage from "react-use-localstorage";
+import { jsonGrammar } from "../examples/json";
+
+function Main() {
+  return <Playground grammar={jsonGrammar} startRule="value" />;
+}
+
+function Playground(props: { grammar: Grammar; startRule: string }) {
+  const [source, setSource] = useLocalStorage(
+    "parserlib-playground-source",
+    ""
+  );
+  const tree = parse(props.grammar, props.startRule, source);
+  console.log(source, tree);
+
+  return (
+    <>
+      <h1>Parserlib Playground</h1>
+
+      <textarea
+        value={source}
+        onChange={(evt) => setSource(evt.target.value)}
+        rows={10}
+        cols={50}
+      />
+
+      <ReactJson
+        name={null}
+        enableClipboard={false}
+        displayObjectSize={false}
+        displayDataTypes={false}
+        src={tree}
+        shouldCollapse={({ name }) => name === "span"}
+      />
+    </>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById("main"));
