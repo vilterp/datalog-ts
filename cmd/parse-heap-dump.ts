874c3d84fc34afe362c2f9d699d7450f2e57f3c2
Pete Vilter <pete.vilter@gmail.com>
2020-08-07 01:54:38 -0400

    add heap dump to csv parser

---
+++ cmd/parse-heap-dump.ts
@@ -0,0 +1,41 @@
+import * as oboe from "oboe";
+import * as fs from "fs";
+
+const stream = oboe(process.stdin);
+
+const nodes = fs.openSync("./nodes.csv", "w");
+const edges = fs.openSync("./edges.csv", "w");
+
+const COLS_NODES = 6;
+const COLS_EDGES = 3;
+
+stream.node(".nodes.*", (val, pathOrHeaders) => {
+  // console.log(val, pathOrHeaders);
+  const idx = parseInt(pathOrHeaders[1]);
+  if (idx % COLS_NODES === 0 && idx > 0) {
+    fs.writeSync(nodes, "\n");
+  } else if (idx > 0) {
+    fs.writeSync(nodes, ",");
+  }
+  fs.writeSync(nodes, val);
+});
+
+stream.node(".edges.*", (val, pathOrHeaders) => {
+  // console.log(val, pathOrHeaders);
+  const idx = parseInt(pathOrHeaders[1]);
+  if (idx % COLS_EDGES === 0) {
+    // console.log("hello", idx);
+    if (idx > 0) {
+      fs.writeSync(edges, "\n");
+    }
+    fs.writeSync(edges, `${idx / COLS_EDGES},`);
+  } else {
+    fs.writeSync(edges, ",");
+  }
+  fs.writeSync(edges, val);
+});
+
+stream.done(() => {
+  fs.closeSync(nodes);
+  fs.closeSync(edges);
+});
