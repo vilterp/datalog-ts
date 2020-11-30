0e35bea7de9281225f549a707f3a01dc1e98ed90
Pete Vilter <pete.vilter@gmail.com>
2020-11-30 11:59:48 -0500

    try plexus
    sucks worse than I remember

--- util/ddTest/visualizers.tsx
+++ util/ddTest/visualizers.tsx
@@ -7,6 +7,15 @@ import { toGraphviz } from "../../incremental/graphviz";
 import { prettyPrintGraph } from "../../graphviz";
 import { DagreReact } from "dagre-reactjs";
 import { toDagre } from "../../incremental/dagre";
+import Digraph from "@jaegertracing/plexus/lib/DirectedGraph";
+import { TEdge, TVertex } from "@jaegertracing/plexus/lib/types";
+import { LayoutManager } from "@jaegertracing/plexus";
+
+const lm = new LayoutManager({
+  useDotEdges: true,
+  rankdir: "TB",
+  ranksep: 1.1,
+});
 
 function EmissionLogViewer(props: { text: string }) {
   const logAndGraph: EmissionLogAndGraph = JSON.parse(props.text);
@@ -16,27 +25,45 @@ function EmissionLogViewer(props: { text: string }) {
   );
   const width = useWindowWidth();
 
+  const graph = toGraphviz(logAndGraph.graph, highlightedNodeID);
+  const nodes: TVertex[] = graph.nodes.map((node) => ({
+    key: node.id,
+    label: node.attrs.label,
+  }));
+  const edges: TEdge[] = graph.edges.map((edge) => ({
+    from: edge.from,
+    to: edge.to,
+  }));
+
   return (
     <div>
+      <Digraph
+        setOnGraph={{
+          style: {
+            fontFamily: "sans-serif",
+            height: "100%",
+            position: "fixed",
+            width: "100%",
+          },
+        }}
+        zoom={true}
+        edges={edges}
+        vertices={nodes}
+        layoutManager={lm}
+      />
       <ul>
-        {logAndGraph.log.map((batch, idx) => {
-          const graph = toDagre(logAndGraph.graph, batch.fromID);
-          return (
-            <li
-              key={idx}
-              onClick={() => setHighlightedNodeID(batch.fromID)}
-              style={{
-                cursor: "pointer",
-                color: highlightedNodeID === batch.fromID ? "red" : "black",
-              }}
-            >
-              {batch.fromID}: {batch.output.map((res) => formatRes(res))}
-              <svg height={600} width={20000}>
-                <DagreReact nodes={graph.nodes} edges={graph.edges} />
-              </svg>
-            </li>
-          );
-        })}
+        {logAndGraph.log.map((batch, idx) => (
+          <li
+            key={idx}
+            onClick={() => setHighlightedNodeID(batch.fromID)}
+            style={{
+              cursor: "pointer",
+              color: highlightedNodeID === batch.fromID ? "red" : "black",
+            }}
+          >
+            {batch.fromID}: {batch.output.map((res) => formatRes(res))}
+          </li>
+        ))}
       </ul>
     </div>
   );
