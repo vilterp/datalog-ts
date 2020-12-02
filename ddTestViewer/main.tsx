a3909c598d59144ab5392d0516439cdc022ca1fe
Pete Vilter <pete.vilter@gmail.com>
2020-12-02 00:16:54 -0500

    turn ddtest viewer back into a viewer

--- ddTestViewer/main.tsx
+++ ddTestViewer/main.tsx
@@ -1,40 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { testSpecs } from "../incremental/ddTests";
-import { zip } from "../util";
+import { parseDDTest } from "../util/ddTest/parser";
+import useLocalStorage from "react-use-localstorage";
+import { VISUALIZERS } from "../util/ddTest/visualizers";
 
 function Main() {
-  return <TestRunner />;
+  return <TestViewer />;
 }
 
-function TestRunner() {
+function TestViewer() {
+  const [testSource, setTestSource] = useLocalStorage(
+    "ddtest-viewer-source",
+    ""
+  );
+  const parsedTest = parseDDTest(testSource);
+
   return (
     <>
-      <h1>DDTest Runner</h1>
-      {testSpecs.map((suite, idx) => {
-        const outputs = suite.func(suite.inputs);
-        const results = zip(suite.inputs, outputs, (input, output) => ({
-          input,
-          output,
-        }));
-        return (
-          <div key={idx}>
-            <h2>{suite.name}</h2>
-            {results.map((result, idx) => (
-              <div key={idx}>
-                <pre>
-                  {result.input}
-                  <br />
-                  ----
-                </pre>
-                {suite.visualizers[result.output.mimeType](
-                  result.output.content
-                )}
-              </div>
-            ))}
-          </div>
-        );
-      })}
+      <h1>DDTest Viewer</h1>
+      <h3>Test Source</h3>
+      <textarea
+        value={testSource}
+        onChange={(evt) => setTestSource(evt.target.value)}
+        style={{ fontFamily: "monospace" }}
+        cols={100}
+        rows={20}
+      />
+      <h3>Viewer</h3>
+      {parsedTest.map((pair, idx) => (
+        <div key={idx}>
+          <pre>
+            {pair.input}
+            <br />
+            ----
+          </pre>
+          {VISUALIZERS[pair.output.mimeType](pair.output.content)}
+        </div>
+      ))}
     </>
   );
 }
