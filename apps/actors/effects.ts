1ee1c0ed1338fc99616eee1bc335226353630d99
Pete Vilter <pete.vilter@gmail.com>
2021-05-13 18:22:13 -0400

    mport communicating actors project

---
+++ apps/actors/effects.ts
@@ -0,0 +1,48 @@
+import { ActorID, ActorResp, IncomingMessage } from "./model";
+
+export function reply<ServerState, Req, Resp>(
+  newSt: ServerState,
+  msg: IncomingMessage<Req>,
+  resp: Resp
+): ActorResp<ServerState, Resp> {
+  return {
+    type: "continue",
+    state: newSt,
+    messages: [{ to: msg.from, msg: resp }],
+  };
+}
+
+export function sleep<State, Msg>(
+  newState: State,
+  durationMS: number
+): ActorResp<State, Msg> {
+  return {
+    type: "sleep",
+    durationMS,
+    state: newState,
+  };
+}
+
+export function send<State, Msg>(
+  newState: State,
+  msg: Msg,
+  to: ActorID
+): ActorResp<State, Msg> {
+  return {
+    type: "continue",
+    state: newState,
+    messages: [{ to, msg }],
+  };
+}
+
+export function update<State, Msg>(newSt: State): ActorResp<State, Msg> {
+  return {
+    type: "continue",
+    state: newSt,
+    messages: [],
+  };
+}
+
+export function exit<State, Message>(): ActorResp<State, Message> {
+  return { type: "exit" };
+}
---
+++ apps/actors/scenarios/lsm.tsx
@@ -0,0 +1,181 @@
+import React from "react";
+import {
+  ActorResp,
+  initialSteps,
+  LoadedTickInitiator,
+  sendUserInput,
+  Trace,
+  UpdateFn,
+} from "../model";
+import * as effects from "../effects";
+import { Scenario } from "../scenario";
+
+// states
+
+export type ClientServerActorState = ServerState | ClientState | UserState;
+
+type ClientState = {
+  type: "ClientState";
+  value: number;
+  status: "saving" | "steady";
+};
+
+type ServerState = { type: "ServerState"; value: number };
+
+type UserState = { type: "UserState"; step: number };
+
+// messages
+
+export type ClientServerMsg = MsgToUser | MsgToClient | MsgToServer;
+
+type MsgToUser = never;
+
+type MsgToClient = "increment" | "decrement" | ServerResp;
+
+type MsgToServer = "increment" | "decrement";
+
+type ServerResp = "ack";
+
+// initial state
+
+export function getInitialState(): Trace<
+  ClientServerActorState,
+  ClientServerMsg
+> {
+  // TODO: something about <god>, lol
+  return initialSteps(update, [
+    {
+      to: "client",
+      from: "<god>",
+      init: {
+        type: "spawned",
+        initialState: { type: "ClientState", value: 0, status: "steady" },
+        spawningTickID: "init",
+      },
+    },
+    {
+      to: "server",
+      from: "<god>",
+      init: {
+        type: "spawned",
+        initialState: { type: "ServerState", value: 0 },
+        spawningTickID: "init",
+      },
+    },
+    {
+      to: "user",
+      from: "<god>",
+      init: {
+        type: "spawned",
+        initialState: { type: "UserState", step: 0 },
+        spawningTickID: "init",
+      },
+    },
+  ]);
+}
+
+// behaviors
+
+export function server(
+  state: ServerState,
+  init: LoadedTickInitiator<ServerState, MsgToServer>
+): ActorResp<ServerState, ServerResp> {
+  switch (init.type) {
+    case "messageReceived": {
+      const msg = init.payload;
+      switch (msg) {
+        case "increment":
+          return effects.reply(
+            { type: "ServerState", value: state.value + 1 },
+            { from: init.from, msg: init.payload },
+            "ack"
+          );
+        case "decrement":
+          return effects.reply(
+            { type: "ServerState", value: state.value - 1 },
+            { from: init.from, msg: init.payload },
+            "ack"
+          );
+        default:
+          return effects.update(state);
+      }
+    }
+    default:
+      return effects.update(state);
+  }
+}
+
+export function client(
+  state: ClientState,
+  init: LoadedTickInitiator<ClientState, MsgToClient>
+): ActorResp<ClientState, MsgToServer> {
+  switch (init.type) {
+    case "messageReceived": {
+      switch (init.payload) {
+        // from user
+        case "increment":
+          return effects.send(
+            { type: "ClientState", value: state.value + 1, status: "saving" },
+            "increment",
+            "server"
+          );
+        case "decrement":
+          return effects.send(
+            { type: "ClientState", value: state.value - 1, status: "saving" },
+            "increment",
+            "server"
+          );
+        // from server
+        case "ack":
+          // unless there are concurrent requests...
+          return effects.update({ ...state, status: "steady" });
+      }
+      break;
+    }
+    default:
+      return effects.update(state);
+  }
+}
+
+// tying it all together
+
+export const update: UpdateFn<ClientServerActorState, ClientServerMsg> = (
+  state,
+  init
+): ActorResp<ClientServerActorState, ClientServerMsg> => {
+  switch (state.type) {
+    case "ClientState":
+      return client(
+        state,
+        init as LoadedTickInitiator<ClientState, MsgToClient>
+      );
+    case "ServerState":
+      return server(
+        state,
+        init as LoadedTickInitiator<ServerState, MsgToServer>
+      );
+    case "UserState":
+      return effects.update(state);
+  }
+};
+
+// ui
+
+type CSTrace = Trace<ClientServerActorState, ClientServerMsg>;
+
+export function ClientServerUI(props: {
+  trace: CSTrace;
+  setTrace: (t: CSTrace) => void;
+}) {
+  return <>yoooo</>;
+}
+
+// scenario
+
+export const scenario: Scenario<ClientServerActorState, ClientServerMsg> = {
+  name: "LSM",
+  id: "lsm",
+  ui: ClientServerUI,
+  update,
+  initialState: getInitialState(),
+};
