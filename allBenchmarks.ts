c5a6930a39e7c24c0051f20f91f8b8f7a30e5ae2
Pete Vilter <pete.vilter@gmail.com>
2021-05-13 23:50:14 -0400

    get benchmark running

---
+++ allBenchmarks.ts
@@ -0,0 +1,28 @@
+import { BenchmarkSpec } from "./util/benchmark";
+import { fpBenchmarks } from "./apps/fp/benchmarks";
+import { uploadResultToAirtable } from "./util/airtable";
+
+const allBenchmarks: { [name: string]: BenchmarkSpec[] } = {
+  fpBenchmarks,
+};
+
+async function runAll() {
+  for (let suiteName of Object.keys(allBenchmarks)) {
+    console.log(suiteName);
+    const suite = allBenchmarks[suiteName];
+    for (let entry of suite) {
+      console.log(`  ${entry.name}`);
+      const res = entry.run();
+      console.log("  ", res);
+      await uploadResultToAirtable(`${suiteName}/${entry.name}`, res);
+    }
+  }
+}
+
+// just cuz we don't have top-level await...
+runAll()
+  .then(() => console.log("done"))
+  .catch((err) => {
+    console.error(err);
+    process.exit(-1);
+  });
