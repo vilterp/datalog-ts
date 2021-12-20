521a2f75f4ffea02d812cf7995382967bfef8940
Pete Vilter <pete.vilter@gmail.com>
2021-12-20 00:59:20 -0500

    first hack at flatten

--- parserlib/flatten.ts
+++ parserlib/flatten.ts
@@ -1,6 +1,20 @@
-import { Rec } from "../core/types";
-import { TraceTree } from "./parser";
+import { rec, int, Rec, Term } from "../core/types";
+import { RuleTree } from "./ruleTree";
 
-export function flatten(tt: TraceTree): Rec[] {
-  throw new Error("TODO");
+export function flatten(tree: RuleTree): Rec[] {
+  const records: Rec[] = [];
+  recur(records, tree);
+  return records;
+}
+
+function recur(records: Rec[], tree: RuleTree): number {
+  const props: { [name: string]: Term } = {};
+  tree.children.forEach((child) => {
+    const childID = recur(records, child);
+    props[child.name] = int(childID);
+  });
+  const id = records.length;
+  props.id = int(id);
+  records.push(rec(tree.name, props));
+  return id;
 }
