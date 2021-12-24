623ceb46c835ea77aff435bcff5292f50c589b2a
Pete Vilter <pete.vilter@gmail.com>
2021-12-24 17:46:44 -0500

    comment out graph thing
    superseded by viz sidebar

--- apps/fiddle/main.tsx
+++ apps/fiddle/main.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { nullLoader } from "../../core/loaders";
-import { Program, Rec, StringLit } from "../../core/types";
-import { language } from "../../core/parser";
 import { LayoutManager } from "@jaegertracing/plexus";
-// TODO(joe): Update import after killing `DirectedGraph`
-import Digraph from "@jaegertracing/plexus/lib/DirectedGraph";
 // @ts-ignore
 import familyDL from "../../core/testdata/family_rules.dl";
-import { uniqBy } from "../../util/util";
 import { Explorer } from "../../uiCommon/explorer";
 import useLocalStorage from "react-use-localstorage";
-import { Collapsible } from "../../uiCommon/generic/collapsible";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
 
 const lm = new LayoutManager({
@@ -25,27 +19,10 @@ function Main() {
   const [source, setSource] = useLocalStorage("fiddle-dl-source", familyDL);
 
   let error = null;
-  let nodes = [];
-  let edges = [];
 
   let interp = new SimpleInterpreter(".", nullLoader) as AbstractInterpreter;
   try {
     interp = interp.evalStr(source)[1];
-    edges = uniqBy(
-      interp.queryStr("edge{from: F, to: T, label: L}").map((res) => ({
-        from: ((res.term as Rec).attrs.from as StringLit).val,
-        to: ((res.term as Rec).attrs.to as StringLit).val,
-        label: ((res.term as Rec).attrs.label as StringLit).val,
-      })),
-      (e) => `${e.from}-${e.to}`
-    );
-    nodes = uniqBy(
-      interp.queryStr("node{id: I, label: L}").map((res) => ({
-        key: ((res.term as Rec).attrs.id as StringLit).val,
-        label: ((res.term as Rec).attrs.label as StringLit).val,
-      })),
-      (n) => n.key
-    );
   } catch (e) {
     error = e.toString();
   }
@@ -64,23 +41,11 @@ function Main() {
       {error ? (
         <>
           <h3>Error</h3>
-          <pre>{error}</pre>
+          <pre style={{ color: "red" }}>{error}</pre>
         </>
       ) : null}
       <h3>Explore</h3>
       <Explorer interp={interp} />
-      <Collapsible
-        heading="Graph"
-        initiallyCollapsed={true}
-        content={
-          <Digraph
-            zoom={true}
-            edges={edges}
-            vertices={nodes}
-            layoutManager={lm}
-          />
-        }
-      />
     </div>
   );
 }
