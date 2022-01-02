d44804eeebadd656b9274fb4f3920064b0625585
Pete Vilter <pete.vilter@gmail.com>
2022-01-01 23:22:24 -0500

    fix up loader

--- core/loaders.ts
+++ core/loaders.ts
@@ -1,14 +1,17 @@
 // throws an exception if it's not there I guess
 // TODO: wish there was a stdlib Result<E, T> type, lol
+
+import { contains } from "../util/util";
+
 // keeping synchronous for now
 export type Loader = (path: string) => string;
 
 export function makeMemoryLoader(files: { [path: string]: string }): Loader {
   return (path) => {
-    const contents = files[path];
-    if (!contents) {
+    if (!contains(Object.keys(files), path)) {
       throw new Error(`not found: ${path}`);
     }
+    const contents = files[path];
     return contents;
   };
 }
