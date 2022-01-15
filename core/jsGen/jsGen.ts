45760edfb5febf4df28936d68e25acc838744d42
Pete Vilter <pete.vilter@gmail.com>
2022-01-15 16:09:59 -0500

    take other relations as arguments

--- core/jsGen/jsGen.ts
+++ core/jsGen/jsGen.ts
@@ -1,15 +1,22 @@
 import { generate } from "astring";
 import { FunctionDeclaration } from "estree";
+import { flatMap } from "../../util/util";
 import { Rule } from "../types";
 
 export function generateJS(rule: Rule): FunctionDeclaration {
+  const rulesUsed = flatMap(rule.body.opts, (andExpr) =>
+    flatMap(andExpr.clauses, (clause) =>
+      clause.type == "Record" ? [clause.relation] : []
+    )
+  );
+
   return {
     type: "FunctionDeclaration",
     id: {
       type: "Identifier",
       name: rule.head.relation,
     },
-    params: [],
+    params: rulesUsed.map((name) => ({ type: "Identifier", name })),
     body: {
       type: "BlockStatement",
       body: [],
