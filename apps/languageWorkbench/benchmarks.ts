2056bad7e30798d96276e5e375a78374d1d2198b
Pete Vilter <pete.vilter@gmail.com>
2022-03-25 22:27:22 -0400

    reduce reps

--- apps/languageWorkbench/benchmarks.ts
+++ apps/languageWorkbench/benchmarks.ts
@@ -1,13 +1,16 @@
 import { BenchmarkSpec, runDDBenchmark } from "../../util/testBench/benchmark";
 import { testLangQuery } from "./ddTest";
 
-export const lwbBenchmarks: BenchmarkSpec[] = ["fp", "dl"].map((lang) => ({
+export const lwbBenchmarks: BenchmarkSpec[] = [
+  { lang: "fp", reps: 100 },
+  { lang: "dl", reps: 20 },
+].map(({ lang, reps }) => ({
   name: lang,
   async run() {
     return runDDBenchmark(
       `apps/languageWorkbench/languages/${lang}/${lang}.dd.txt`,
       testLangQuery,
-      100
+      reps
     );
   },
 }));
