1d98644ecfa7ca1142e01f55515bb1a6afb93795
Pete Vilter <pete.vilter@gmail.com>
2022-03-25 22:04:15 -0400

    add dl benchmark

--- apps/languageWorkbench/benchmarks.ts
+++ apps/languageWorkbench/benchmarks.ts
@@ -1,15 +1,13 @@
 import { BenchmarkSpec, runDDBenchmark } from "../../util/testBench/benchmark";
 import { testLangQuery } from "./ddTest";
 
-export const lwbBenchmarks: BenchmarkSpec[] = [
-  {
-    name: "fp",
-    async run() {
-      return runDDBenchmark(
-        "apps/languageWorkbench/languages/fp/fp.dd.txt",
-        testLangQuery,
-        100
-      );
-    },
+export const lwbBenchmarks: BenchmarkSpec[] = ["fp", "dl"].map((lang) => ({
+  name: lang,
+  async run() {
+    return runDDBenchmark(
+      `apps/languageWorkbench/languages/${lang}/${lang}.dd.txt`,
+      testLangQuery,
+      100
+    );
   },
-];
+}));
