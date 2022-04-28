fa9a173d23766ef2eec20ee51a9e9fb8ba97c2cc
Pete Vilter <pete.vilter@gmail.com>
2022-04-27 21:06:15 -0400

    add explorer

--- apps/sim/main.tsx
+++ apps/sim/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { nullLoader } from "../../core/loaders";
+import { SimpleInterpreter } from "../../core/simple/interpreter";
+import { Explorer } from "../../uiCommon/explorer";
+
+const interp = new SimpleInterpreter(".", nullLoader);
 
 function Main() {
-  return <p>Hello world</p>;
+  return (
+    <>
+      <h1>Simulator</h1>
+      <Explorer interp={interp} />
+    </>
+  );
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"));
