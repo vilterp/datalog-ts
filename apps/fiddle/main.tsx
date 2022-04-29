0ce920d42c511cbed8276773a53279c3cb3f09e6
Pete Vilter <pete.vilter@gmail.com>
2022-04-28 23:56:51 -0400

    switch over to lingo-powered editor in Fiddle

--- apps/fiddle/main.tsx
+++ apps/fiddle/main.tsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { nullLoader } from "../../core/loaders";
 // @ts-ignore
 import familyDL from "../../core/testdata/family_rules.dl";
 import { Explorer } from "../../uiCommon/explorer";
-import useLocalStorage from "react-use-localstorage";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
 import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { WrappedCodeEditor } from "../../uiCommon/ide/datalogPowered/wrappedCodeEditor";
+import { initialEditorState } from "../../uiCommon/ide/types";
+import { LANGUAGES } from "../languageWorkbench/languages";
+// @ts-ignore
+import commonThemeCSS from "../languageWorkbench/commonTheme.css";
+import { useJSONLocalStorage } from "../../uiCommon/generic/hooks";
+
+console.log({ LANGUAGES });
 
 function Main() {
-  const [editorState, setEditorState] = useLocalStorage(
-    "fiddle-dl-source",
-    familyDL
+  const [editorState, setEditorState] = useJSONLocalStorage(
+    "datalog-fiddle-editor-state",
+    initialEditorState(familyDL)
   );
 
   let error = null;
 
   let interp: AbstractInterpreter = new SimpleInterpreter(".", nullLoader);
   try {
-    interp = interp.evalStr(source)[1];
+    interp = interp.evalStr(editorState.source)[1];
   } catch (e) {
     error = e.toString();
   }
@@ -28,18 +34,12 @@ function Main() {
     <div>
       <h1>Datalog Fiddle</h1>
       <WrappedCodeEditor
-        datalog={XXX}
-        grammar={XXX}
-        highlightCSS={""}
-        editorState={{
-          cursorPos: 0,
-          source: "",
-          selectedSuggIdx: 0,
-        }}
-        setEditorState={function (st: EditorState): void {
-          throw new Error("Function not implemented.");
-        }}
-        lang={""}
+        datalog={LANGUAGES.datalog.datalog}
+        grammar={LANGUAGES.datalog.grammar}
+        highlightCSS={commonThemeCSS}
+        lang="datalog"
+        editorState={editorState}
+        setEditorState={setEditorState}
       />
       <br />
       {error ? (
