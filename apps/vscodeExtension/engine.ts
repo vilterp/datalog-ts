c1366b4d0892a9f81cd512d8a8a2dbae49eda04a
Pete Vilter <pete.vilter@gmail.com>
2022-05-04 23:27:38 -0400

    factor out getInterp

--- apps/vscodeExtension/engine.ts
+++ apps/vscodeExtension/engine.ts
@@ -1,12 +1,16 @@
 import * as vscode from "vscode";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
 import { constructInterp } from "../../uiCommon/ide/dlPowered/interp";
-import { LANGUAGES } from "../../uiCommon/ide/dlPowered/languages";
+import {
+  LANGUAGES,
+  LanguageSpec,
+} from "../../uiCommon/ide/dlPowered/languages";
 import { LOADER, mainDL } from "../../uiCommon/ide/dlPowered/common";
 import { Rec } from "../../core/types";
 import { dlToSpan } from "../../uiCommon/ide/types";
 import { ppt } from "../../core/pretty";
 import { lineAndColFromIdx } from "../../util/indexToLineCol";
+import { AbstractInterpreter } from "../../core/abstractInterpreter";
 
 export function refreshDiagnostics(
   doc: vscode.TextDocument,
@@ -15,15 +19,9 @@ export function refreshDiagnostics(
   console.log("refresh diagnostics");
   const source = doc.getText();
 
-  const { finalInterp } = constructInterp({
-    initInterp: new SimpleInterpreter(".", LOADER),
-    builtinSource: mainDL,
-    dlSource: LANGUAGES.datalog.datalog,
-    grammarSource: LANGUAGES.datalog.grammar,
-    langSource: source,
-  });
+  const interp = getInterp(LANGUAGES.datalog, source);
 
-  const problems = finalInterp.queryStr("tc.Problem{}");
+  const problems = interp.queryStr("tc.Problem{}");
   console.log(
     "problems",
     problems.map((r) => ppt(r.term))
@@ -43,4 +41,18 @@ function problemToDiagnostic(source: string, rec: Rec): vscode.Diagnostic {
 
   const range = new vscode.Range(from.line, from.col, to.line, to.col);
   return new vscode.Diagnostic(range, ppt(rec.attrs.desc));
+}
+
+function getInterp(
+  language: LanguageSpec,
+  source: string
+): AbstractInterpreter {
+  const { finalInterp } = constructInterp({
+    initInterp: new SimpleInterpreter(".", LOADER),
+    builtinSource: mainDL,
+    dlSource: language.datalog,
+    grammarSource: language.grammar,
+    langSource: source,
+  });
+  return finalInterp;
 }
