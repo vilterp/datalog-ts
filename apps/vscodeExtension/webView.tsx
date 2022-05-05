f237921f199feed3f7706d87e61e627b78fb270a
Pete Vilter <pete.vilter@gmail.com>
2022-05-05 15:02:48 -0400

    put dl that gets sent into interp

--- apps/vscodeExtension/webView.tsx
+++ apps/vscodeExtension/webView.tsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { nullLoader } from "../../core/loaders";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
 import { Explorer } from "../../uiCommon/explorer";
 import { MessageToWebView } from "./types";
 
 export function Main() {
-  const interp = new SimpleInterpreter(".", nullLoader);
+  const [dlSource, setDLSource] = useState("");
 
-  // TODO: subscribe to messages; put into interpreter...
+  useEffect(() => {
+    const listener = (evt) => {
+      const msg = evt.data as MessageToWebView;
+      switch (msg.type) {
+        case "ContentsUpdated":
+          setDLSource(msg.text);
+          break;
+        default:
+          console.log("unknown message:", msg);
+          break;
+      }
+    };
+    window.addEventListener("message", listener);
+    return () => {
+      window.removeEventListener("message", listener);
+    };
+  });
 
-  return <Explorer interp={interp} />;
+  let error = "";
+  let interp: AbstractInterpreter = new SimpleInterpreter(".", nullLoader);
+  try {
+    interp = interp.evalStr(dlSource)[1];
+  } catch (e) {
+    error = e.toString();
+  }
+
+  return (
+    <>
+      {error ? <pre style={{ backgroundColor: "red" }}>{error}</pre> : null}
+      <Explorer interp={interp} />
+    </>
+  );
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"));
-
-window.addEventListener("message", (evt) => {
-  const msg = evt.data as MessageToWebView;
-  switch (msg.type) {
-    case "ContentsUpdated":
-      console.log("message:", msg.text);
-      break;
-    default:
-      console.log("unknown message:", msg);
-      break;
-  }
-});
