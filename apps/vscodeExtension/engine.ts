a57f31a568fed157042feb426fdbb8f4264206da
Pete Vilter <pete.vilter@gmail.com>
2022-05-04 23:38:04 -0400

    remove logs

--- apps/vscodeExtension/engine.ts
+++ apps/vscodeExtension/engine.ts
@@ -1,11 +1,8 @@
 import * as vscode from "vscode";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
-import { constructInterp } from "../../uiCommon/ide/dlPowered/interp";
-import {
-  LANGUAGES,
-  LanguageSpec,
-} from "../../uiCommon/ide/dlPowered/languages";
-import { LOADER, mainDL } from "../../uiCommon/ide/dlPowered/common";
+import { constructInterp } from "../../languageWorkbench/interp";
+import { LANGUAGES, LanguageSpec } from "../../languageWorkbench/languages";
+import { LOADER, mainDL } from "../../languageWorkbench/common";
 import { Rec } from "../../core/types";
 import { dlToSpan } from "../../uiCommon/ide/types";
 import { ppt } from "../../core/pretty";
@@ -16,16 +13,11 @@ export function refreshDiagnostics(
   doc: vscode.TextDocument,
   diagnostics: vscode.DiagnosticCollection
 ) {
-  console.log("refresh diagnostics");
   const source = doc.getText();
 
   const interp = getInterp(LANGUAGES.datalog, source);
 
   const problems = interp.queryStr("tc.Problem{}");
-  console.log(
-    "problems",
-    problems.map((r) => ppt(r.term))
-  );
   const diags = problems.map((res) =>
     problemToDiagnostic(source, res.term as Rec)
   );
@@ -36,8 +28,6 @@ function problemToDiagnostic(source: string, rec: Rec): vscode.Diagnostic {
   const span = dlToSpan(rec.attrs.span as Rec);
   const from = lineAndColFromIdx(source, span.from);
   const to = lineAndColFromIdx(source, span.to);
-
-  console.log("problem", { span, from, to });
 
   const range = new vscode.Range(from.line, from.col, to.line, to.col);
   return new vscode.Diagnostic(range, ppt(rec.attrs.desc));
