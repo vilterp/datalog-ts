50dab013f071f84c856cc01940f4fe4b3970644f
Pete Vilter <pete.vilter@gmail.com>
2022-05-05 14:35:07 -0400

    get explorer rendering
    the colors are all wrong lol

--- apps/vscodeExtension/webView.tsx
+++ apps/vscodeExtension/webView.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { nullLoader } from "../../core/loaders";
+import { SimpleInterpreter } from "../../core/simple/interpreter";
+import { Explorer } from "../../uiCommon/explorer";
 
 export function Main() {
-  return <p>Hello web view!</p>;
+  const interp = new SimpleInterpreter(".", nullLoader);
+  return <Explorer interp={interp} />;
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"));
