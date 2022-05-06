73da2136ea2c0c382a92dd5560cb6aef260fab81
Pete Vilter <pete.vilter@gmail.com>
2022-05-06 19:08:38 -0400

    update fp editor
    somehow got lost in the rebase

--- apps/fp/main.tsx
+++ apps/fp/main.tsx
@@ -1,52 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { language as fpLanguage } from "./parser";
-import { flatten } from "./flatten";
+import { SimpleInterpreter } from "../../core/simple/interpreter";
 import { Explorer } from "../../uiCommon/explorer";
 import { CollapsibleWithHeading } from "../../uiCommon/generic/collapsible";
-import {
-  CodeEditor,
-  loadInterpreter,
-} from "../../uiCommon/ide/parsimmonPowered/codeEditor";
 import { useJSONLocalStorage } from "../../uiCommon/generic/hooks";
 import { initialEditorState } from "../../uiCommon/ide/types";
-// @ts-ignore
-import highlightCSS from "./highlight.css";
-import { LOADER } from "./dl";
-import { getSuggestions } from "./suggestions";
-import { IncrementalInterpreter } from "../../core/incremental/interpreter";
-import { AbstractInterpreter } from "../../core/abstractInterpreter";
+import { LingoEditor } from "../../uiCommon/ide/editor";
+import { LANGUAGES } from "../../languageWorkbench/languages";
+import { addCursor, constructInterp } from "../../languageWorkbench/interp";
+import { LOADER } from "../../languageWorkbench/commonDL";
+
+const INIT_INTERP = new SimpleInterpreter(".", LOADER);
 
 function Main() {
-  let interp = new IncrementalInterpreter(".", LOADER) as AbstractInterpreter;
-
   const [editorState, setEditorState] = useJSONLocalStorage(
     "fp-editor-state",
     initialEditorState("let x = 2 in intToString(x)")
   );
 
-  interp = interp.doLoad("main.dl");
-  const { interp: newInterp, error } = loadInterpreter(
-    interp,
-    editorState,
-    fpLanguage.expr,
-    flatten
+  const { interp: withoutCursor } = constructInterp(
+    INIT_INTERP,
+    LANGUAGES.fp,
+    editorState.source
   );
-  interp = newInterp;
+  const interp = addCursor(withoutCursor, editorState.cursorPos);
 
   return (
     <div>
       <h1>Datalog Typechecker</h1>
       <h2>Source</h2>
-      <CodeEditor
-        lang="fp"
-        interp={interp}
-        getSuggestions={getSuggestions}
-        highlightCSS={highlightCSS}
+      <LingoEditor
         editorState={editorState}
         setEditorState={setEditorState}
-        loadError={error}
-        autofocus={true}
+        langSpec={LANGUAGES.fp}
       />
       <CollapsibleWithHeading
         heading="Facts &amp; Rules"
