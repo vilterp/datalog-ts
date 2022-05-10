9531b23d1a081005684f6ca4f71b8037d505f305
Pete Vilter <pete.vilter@gmail.com>
2022-05-10 08:17:03 -0400

    don't register language support as much
    idk, feel weird about not putting interpGetter in the deps array when it does get passed in

--- uiCommon/ide/editorInner.tsx
+++ uiCommon/ide/editorInner.tsx
@@ -48,7 +48,7 @@ export function LingoEditorInner(props: {
     // make sure to register support for new langauge when we switch
     // languages.
     registerLanguageSupport(monacoRef.current, props.langSpec, interpGetter);
-  }, [props.langSpec, monacoRef.current, interpGetter]);
+  }, [props.langSpec, monacoRef.current]);
 
   function updateMarkers(editor: monaco.editor.ICodeEditor) {
     const model = editor.getModel();
