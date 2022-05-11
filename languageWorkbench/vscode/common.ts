a901d8f88df047be6ec791743fef1a52ae2b0a0f
Pete Vilter <pete.vilter@gmail.com>
2022-05-11 00:37:34 -0400

    add a reducer

--- languageWorkbench/vscode/common.ts
+++ languageWorkbench/vscode/common.ts
@@ -1,9 +1,86 @@
 import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
 import { rec } from "../../core/types";
+import { mapObj } from "../../util/util";
 import { LOADER } from "../commonDL";
 import { constructInterp } from "../interp";
 import { LanguageSpec } from "../languages";
+
+export type State = {
+  [documentURI: string]: {
+    interp: AbstractInterpreter;
+    source: string;
+    langSpec: LanguageSpec;
+  };
+};
+
+export const initialState: State = {};
+
+export type Action =
+  | {
+      type: "CreateDoc";
+      uri: string;
+      initSource: string;
+      langSpec: LanguageSpec;
+    }
+  | {
+      type: "EditDoc";
+      uri: string;
+      newSource: string;
+      // TODO: diff events
+    }
+  | { type: "ChangeLang"; newLangSpec: LanguageSpec };
+
+export function update(state: State, action: Action): State {
+  switch (action.type) {
+    case "EditDoc": {
+      const current = state[action.uri];
+      const res = constructInterp(
+        INIT_INTERP,
+        current.langSpec,
+        action.newSource
+      );
+      // TODO: something with errors from constructing interp
+      // TODO: process incremental updates. lol
+      return {
+        ...state,
+        [action.uri]: {
+          ...current,
+          interp: res.interp,
+          source: action.newSource,
+        },
+      };
+    }
+    case "ChangeLang": {
+      // TODO: register new lang with Monaco
+      return mapObj(state, (docURI, docState) => {
+        const newInterp = constructInterp(
+          INIT_INTERP,
+          action.newLangSpec,
+          docState.source
+        );
+        return {
+          interp: newInterp.interp,
+          langSpec: action.newLangSpec,
+          source: docState.source,
+        };
+      });
+    }
+    case "CreateDoc":
+      return {
+        ...state,
+        [action.uri]: {
+          langSpec: action.langSpec,
+          source: action.initSource,
+          interp: constructInterp(
+            INIT_INTERP,
+            action.langSpec,
+            action.initSource
+          ).interp,
+        },
+      };
+  }
+}
 
 export type InterpAndSource = { interp: AbstractInterpreter; source: string };
 
@@ -32,15 +109,6 @@ export const TOKEN_TYPES = [
 ];
 
 export const INIT_INTERP = new SimpleInterpreter(".", LOADER);
-
-export function getInterp(
-  language: LanguageSpec,
-  source: string
-): AbstractInterpreter {
-  const res = constructInterp(INIT_INTERP, language, source);
-  // TODO: something with errors if they're there
-  return res.interp;
-}
 
 // TODO: parameterize by language
 export const GLOBAL_SCOPE = rec("global", {});
