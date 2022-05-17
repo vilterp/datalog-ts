434b0fa1e06ed9f24219652e1d382110bf71c272
Pete Vilter <pete.vilter@gmail.com>
2022-05-17 01:09:46 -0400

    start calling childByName

--- languageWorkbench/parserlib/gen/genExtractor.ts
+++ languageWorkbench/parserlib/gen/genExtractor.ts
@@ -1,8 +1,8 @@
 import { Grammar, Rule } from "../grammar";
 import { Program, FunctionDeclaration } from "estree";
 import { generate } from "astring";
-import { mapObjToList } from "../../../util/util";
-import { jsBlock, jsIdent } from "./astHelpers";
+import { flatMap, mapListToObj, mapObjToList, uniq } from "../../../util/util";
+import { jsBlock, jsCall, jsIdent, jsObj, jsStr } from "./astHelpers";
 
 export function genExtractorStr(grammar: Grammar) {
   const program = genExtractor(grammar);
@@ -17,11 +17,49 @@ export function genExtractor(grammar: Grammar): Program {
   };
 }
 
-function genRule(name: string, body: Rule): FunctionDeclaration {
+function refsInRule(rule: Rule): string[] {
+  switch (rule.type) {
+    case "Char":
+      return [];
+    case "Choice":
+      return flatMap(rule.choices, refsInRule);
+    case "Ref":
+      return [rule.name];
+    case "RepSep":
+      return [...refsInRule(rule.rep), ...refsInRule(rule.sep)];
+    case "Sequence":
+      return flatMap(rule.items, refsInRule);
+    case "Text":
+      return [];
+  }
+}
+
+function genRule(name: string, rule: Rule): FunctionDeclaration {
+  const refs = refsInRule(rule);
+  // TODO: check for dups
+  if (uniq(refs).length !== refs.length) {
+    throw new Error(`refs in rule have to be unique. got ${refs}`);
+  }
+  // ....
   return {
     type: "FunctionDeclaration",
     id: jsIdent(`extract_${name}`),
-    params: [],
-    body: jsBlock([]),
+    params: [jsIdent("node")],
+    body: jsBlock([
+      {
+        type: "ReturnStatement",
+        argument: jsObj(
+          mapListToObj(
+            refs.map((name) => ({
+              key: name,
+              value: jsCall(jsIdent("childByName"), [
+                jsIdent("node"),
+                jsStr(name),
+              ]),
+            }))
+          )
+        ),
+      },
+    ]),
   };
 }
