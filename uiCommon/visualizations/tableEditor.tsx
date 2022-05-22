896a7ec34eca604b252a46b03c7a4a41eb11ec4f
Pete Vilter <pete.vilter@gmail.com>
2022-05-22 16:07:51 -0400

    create new buttons

--- uiCommon/visualizations/tableEditor.tsx
+++ uiCommon/visualizations/tableEditor.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ppt } from "../../core/pretty";
-import { Rec } from "../../core/types";
+import { Array, Rec } from "../../core/types";
 import { VizArgs, VizTypeSpec } from "./typeSpec";
 
 // TODO: shouldn't the normal table just be an editor?
@@ -25,14 +25,32 @@ function TableEditor(props: VizArgs) {
   }
 
   // get news
+  let news: Rec[] = [];
+  let newsError: string | null = null;
+  try {
+    news = (props.spec.attrs.new as Array).items.map((item) => item as Rec);
+  } catch (e) {
+    newsError = e.toString();
+  }
 
   return (
-    <ul>
-      {data.map((rec) => (
-        <li key={ppt(rec)}>
-          <pre>{ppt(rec)}</pre>
-        </li>
-      ))}
-    </ul>
+    <>
+      Data
+      <ul>
+        {data.map((rec) => (
+          <li key={ppt(rec)}>
+            <pre>{ppt(rec)}</pre>
+          </li>
+        ))}
+      </ul>
+      Create new:
+      <ul>
+        {news.map((newRec) => (
+          <li key={ppt(newRec)}>
+            <button>{ppt(newRec)}</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
