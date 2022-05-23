d2d41785ffb5a1495221bd86bcee7f2a1ec783c4
Pete Vilter <pete.vilter@gmail.com>
2022-05-22 21:28:24 -0400

    move stuff around in core/types.ts

--- core/types.ts
+++ core/types.ts
@@ -1,6 +1,10 @@
+// === DB Contents ===
+
 export type Relation =
   | { type: "Table"; name: string }
   | { type: "Rule"; name: string; rule: Rule };
+
+// === Results ===
 
 export interface Res {
   term: Term;
@@ -8,49 +12,9 @@ export interface Res {
   trace: Trace;
 }
 
-// traces
-
-export type Trace =
-  | { type: "AndTrace"; sources: Res[] }
-  | { type: "MatchTrace"; fact: Res; match: Rec } // TODO: fact isn't used, since it's always just baseFact
-  | {
-      type: "RefTrace";
-      refTerm: Rec;
-      invokeLoc: InvocationLocation; // where in the calling rule this was
-      innerRes: Res;
-      mappings: VarMappings;
-    }
-  | { type: "NegationTrace"; negatedTerm: Term }
-  | { type: "AggregationTrace"; aggregatedResults: Res[] }
-  | { type: "BaseFactTrace" }
-  | { type: "LiteralTrace" }
-  | { type: "VarTrace" }
-  | { type: "BinExprTrace" };
-
-export const literalTrace: Trace = { type: "LiteralTrace" };
-
-export const varTrace: Trace = { type: "VarTrace" };
-
-export const baseFactTrace: Trace = { type: "BaseFactTrace" };
-
-export const binExprTrace: Trace = { type: "BinExprTrace" };
-
-export type InvocationLocation = RulePathSegment[];
-
-export type RulePathSegment =
-  | { type: "OrOpt"; idx: number }
-  | { type: "AndClause"; idx: number }
-  | { type: "Negation" }
-  | { type: "Aggregation" };
-
-export type ScopePath = { name: string; invokeLoc: InvocationLocation }[];
-
-// gah this should be derived by the language
-export function scopePathEq(left: ScopePath, right: ScopePath): boolean {
-  return JSON.stringify(left) === JSON.stringify(right);
-}
-
 export type Bindings = { [key: string]: Term };
+
+// === Rules ===
 
 export interface Rule {
   // should maybe be an Or of multiple (head, And[]) pairs
@@ -72,6 +36,8 @@ type Aggregation = {
   varNames: string[];
   record: Rec;
 };
+
+// === Terms ===
 
 export type Term = Rec | StringLit | Var | AndClause | Bool | Int | Array;
 
@@ -157,6 +123,48 @@ export const relationalFalse: Term[] = [];
 
 // inner to outer (?)
 export type VarMappings = { [from: string]: string };
+
+// === Traces ===
+
+export type Trace =
+  | { type: "AndTrace"; sources: Res[] }
+  | { type: "MatchTrace"; fact: Res; match: Rec } // TODO: fact isn't used, since it's always just baseFact
+  | {
+      type: "RefTrace";
+      refTerm: Rec;
+      invokeLoc: InvocationLocation; // where in the calling rule this was
+      innerRes: Res;
+      mappings: VarMappings;
+    }
+  | { type: "NegationTrace"; negatedTerm: Term }
+  | { type: "AggregationTrace"; aggregatedResults: Res[] }
+  | { type: "BaseFactTrace" }
+  | { type: "LiteralTrace" }
+  | { type: "VarTrace" }
+  | { type: "BinExprTrace" };
+
+export const literalTrace: Trace = { type: "LiteralTrace" };
+
+export const varTrace: Trace = { type: "VarTrace" };
+
+export const baseFactTrace: Trace = { type: "BaseFactTrace" };
+
+export const binExprTrace: Trace = { type: "BinExprTrace" };
+
+export type InvocationLocation = RulePathSegment[];
+
+export type RulePathSegment =
+  | { type: "OrOpt"; idx: number }
+  | { type: "AndClause"; idx: number }
+  | { type: "Negation" }
+  | { type: "Aggregation" };
+
+export type ScopePath = { name: string; invokeLoc: InvocationLocation }[];
+
+// gah this should be derived by the language
+export function scopePathEq(left: ScopePath, right: ScopePath): boolean {
+  return JSON.stringify(left) === JSON.stringify(right);
+}
 
 // TODO: bindings can be at any level
 export type TermWithBindings =
