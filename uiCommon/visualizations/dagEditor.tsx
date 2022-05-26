d323115cb7ccc7b3ee7bab096dc60f93aafafa89
Pete Vilter <pete.vilter@gmail.com>
2022-05-26 15:25:55 -0400

    make nodes draggable

--- uiCommon/visualizations/dagEditor.tsx
+++ uiCommon/visualizations/dagEditor.tsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { VizArgs, VizTypeSpec } from "./typeSpec";
-import ReactFlow from "react-flow-renderer";
+import ReactFlow, {
+  applyEdgeChanges,
+  applyNodeChanges,
+  Node,
+  Edge,
+} from "react-flow-renderer";
 
 export const dagEditor: VizTypeSpec = {
   name: "DAG Editor",
@@ -12,14 +17,28 @@ function DAGEditor(props: VizArgs) {
   const [nodes, setNodes] = useState(INITIAL_NODES);
   const [edges, setEdges] = useState(initialEdges);
 
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+
   return (
     <div style={{ width: 500, height: 400 }}>
-      <ReactFlow nodes={nodes} edges={edges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      />
     </div>
   );
 }
 
-const INITIAL_NODES = [
+const INITIAL_NODES: Node[] = [
   {
     id: "1",
     type: "input",
@@ -41,7 +60,7 @@ const INITIAL_NODES = [
   },
 ];
 
-const initialEdges = [
+const initialEdges: Edge[] = [
   { id: "e1-2", source: "1", target: "2" },
   { id: "e2-3", source: "2", target: "3", animated: true },
 ];
