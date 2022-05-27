cbbd138e43197194619336c1eae588cba3aee69c
Pete Vilter <pete.vilter@gmail.com>
2022-05-26 20:12:16 -0400

    statements for node change

--- uiCommon/visualizations/dagEditor.tsx
+++ uiCommon/visualizations/dagEditor.tsx
@@ -10,8 +10,9 @@ import ReactFlow, {
   NodeChange,
   Connection,
 } from "react-flow-renderer";
-import { Int, Rec, StringLit } from "../../core/types";
+import { int, Int, Rec, Statement, StringLit } from "../../core/types";
 import { fastPPT } from "../../core/fastPPT";
+import { flatMap } from "../../util/util";
 
 export const dagEditor: VizTypeSpec = {
   name: "DAG Editor",
@@ -22,75 +23,98 @@ export const dagEditor: VizTypeSpec = {
 function DAGEditor(props: VizArgs) {
   // const [nodes, setNodes] = useState(INITIAL_NODES);
   // const [edges, setEdges] = useState(initialEdges);
+  let nodeRecords: Rec[] = [];
+  let edgeRecords: Rec[] = [];
+  let error: string | null = null;
+
+  try {
+    const nodesQuery = props.spec.attrs.nodes as Rec;
+    const edgesQuery = props.spec.attrs.edges as Rec;
+
+    nodeRecords = props.interp.queryRec(nodesQuery).map((r) => r.term as Rec);
+    edgeRecords = props.interp.queryRec(edgesQuery).map((r) => r.term as Rec);
+  } catch (e) {
+    error = e.toString();
+  }
 
   const onNodesChange = useCallback(
     (changes: NodeChange[]) => {
+      const statements: Statement[] = flatMap(changes, (change) => {
+        if (change.type !== "position") {
+          console.warn("change type not supported:", change);
+          return [];
+        }
+        if (!change.position) {
+          return [];
+        }
+        const rec = nodeRecords.find(
+          (rec) => change.id === fastPPT(rec.attrs.id)
+        );
+        if (!rec) {
+          throw new Error(`node not found for change ${change.id}`);
+        }
+        const updatedRec: Rec = {
+          ...rec,
+          attrs: {
+            x: int(change.position.x),
+            y: int(change.position.y),
+          },
+        };
+        return [
+          { type: "Delete", record: rec },
+          { type: "Fact", record: updatedRec },
+        ];
+      });
       console.log("nodes changes", changes);
       // setNodes((nds) => applyNodeChanges(changes, nds));
+      props.runStatements(statements);
     },
-    []
-    // [setNodes]
+    [nodeRecords, props.runStatements]
   );
   const onEdgesChange = useCallback(
     (changes: EdgeChange[]) => {
       console.log("edges changes", changes);
       // setEdges((eds) => applyEdgeChanges(changes, eds));
     },
-    []
-    // [setEdges]
+    [props.runStatements]
   );
   const onConnect = useCallback(
     (connection: Connection) => {
       console.log("onConnect", connection);
       // setEdges((eds) => addEdge(connection, eds));
     },
-    []
-    // [setEdges]
+    [props.runStatements]
   );
 
-  try {
-    const nodesQuery = props.spec.attrs.nodes as Rec;
-    const edgesQuery = props.spec.attrs.edges as Rec;
+  const nodes: Node[] = nodeRecords.map((rec) => {
+    return {
+      id: fastPPT(rec.attrs.id),
+      data: { label: (rec.attrs.label as StringLit).val },
+      position: {
+        x: (rec.attrs.x as Int).val,
+        y: (rec.attrs.y as Int).val,
+      },
+    };
+  });
+  const edges: Edge[] = edgeRecords.map((rec) => {
+    return {
+      id: fastPPT(rec.attrs.id),
+      source: fastPPT(rec.attrs.from),
+      target: fastPPT(rec.attrs.to),
+    };
+  });
 
-    const rawNodes = props.interp.queryRec(nodesQuery);
-    const rawEdges = props.interp.queryRec(edgesQuery);
-
-    const nodes: Node[] = rawNodes.map((rawNode) => {
-      const rec = rawNode.term as Rec;
-      return {
-        id: fastPPT(rec.attrs.id),
-        data: { label: (rec.attrs.label as StringLit).val },
-        position: {
-          x: (rec.attrs.x as Int).val,
-          y: (rec.attrs.y as Int).val,
-        },
-      };
-    });
-    const edges: Edge[] = rawEdges.map((rawEdge) => {
-      const rec = rawEdge.term as Rec;
-      return {
-        id: fastPPT(rec.attrs.id),
-        source: fastPPT(rec.attrs.from),
-        target: fastPPT(rec.attrs.to),
-      };
-    });
-
-    console.log("DagEditor", { nodes, edges });
-
-    return (
-      <div style={{ width: 500, height: 400 }}>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-        />
-      </div>
-    );
-  } catch (e) {
-    return <pre style={{ color: "red" }}>Error: {e.toString()}</pre>;
-  }
+  return (
+    <div style={{ width: 500, height: 400 }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      />
+    </div>
+  );
 }
 
 const INITIAL_NODES: Node[] = [
