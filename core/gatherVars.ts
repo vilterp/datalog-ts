9ddc5b08c6cf03fc44e3efd1a99b810d5261b585
Pete Vilter <pete.vilter@gmail.com>
2022-09-05 18:44:47 -0400

    gather vars

---
+++ core/gatherVars.ts
@@ -0,0 +1,68 @@
+import {
+  flatMap,
+  flatMapObjToList,
+  mapObj,
+  mapObjToList,
+  uniq,
+} from "../util/util";
+import { Conjunct, Term } from "./types";
+
+export function gatherVars(conjuncts: Conjunct[]): string[] {
+  const all = flatMap(conjuncts, (conj) => gatherVarsTerm(conj));
+  return uniq(all);
+}
+
+function gatherVarsTerm(term: Term): string[] {
+  switch (term.type) {
+    case "Var":
+      return [term.name];
+    case "Array":
+      return flatMap(term.items, gatherVarsTerm);
+    case "Dict":
+      return flatMapObjToList(term.map, (_, subTerm) =>
+        gatherVarsTerm(subTerm)
+      );
+    case "Record":
+      return flatMapObjToList(term.attrs, (_, subTerm) =>
+        gatherVarsTerm(subTerm)
+      );
+    // TODO: these probably shouldn't be terms, but alas...
+    case "Aggregation":
+      return gatherVarsTerm(term.record);
+    case "Negation":
+      return gatherVarsTerm(term.record);
+    default:
+      return [];
+  }
+}
+
+export function pathToVar(term: Term, varName: string): string[] {
+  switch (term.type) {
+    case "Var":
+      return term.name === varName ? [] : null;
+    case "Array":
+      return (
+        term.items.map((t) => pathToVar(t, varName)).find((r) => r !== null) ||
+        null
+      );
+    case "Dict":
+      return (
+        mapObjToList(term.map, (_, subTerm) =>
+          pathToVar(subTerm, varName)
+        ).find((r) => r !== null) || null
+      );
+    case "Record":
+      return (
+        mapObjToList(term.attrs, (_, subTerm) =>
+          pathToVar(subTerm, varName)
+        ).find((r) => r !== null) || null
+      );
+    // TODO: these probably shouldn't be terms, but alas...
+    case "Aggregation":
+      return pathToVar(term.record, varName);
+    case "Negation":
+      return pathToVar(term.record, varName);
+    default:
+      return [];
+  }
+}
