c03dea48fe03e94a60814e4ea310b6a42bf3de6b
Pete Vilter <pete.vilter@gmail.com>
2022-09-13 17:11:28 -0400

    turn nodes orange when overlapping

--- uiCommon/dl/ruleGraphEditor/ruleGraphEditor.tsx
+++ uiCommon/dl/ruleGraphEditor/ruleGraphEditor.tsx
@@ -8,6 +8,7 @@ import {
   combineNodes,
   JOIN_VAR_NODE_RADIUS,
 } from "./model";
+import { distance } from "../../../util/geom";
 
 export function RuleGraphEditor(props: {
   ruleGraph: RuleGraph;
@@ -15,6 +16,15 @@ export function RuleGraphEditor(props: {
 }) {
   const svgRef = useRef();
   const [draggingID, setDraggingID] = useState<string | null>(null);
+  const draggingNode = draggingID ? props.ruleGraph.nodes[draggingID] : null;
+  const nodesOverlappingDraggingNode = draggingNode
+    ? Object.entries(props.ruleGraph.nodes)
+        .filter(
+          ([id, node]) =>
+            distance(draggingNode.pos, node.pos) < JOIN_VAR_NODE_RADIUS
+        )
+        .map(([id, node]) => id)
+    : [];
 
   return (
     <svg
@@ -66,6 +76,8 @@ export function RuleGraphEditor(props: {
       <g>
         {Object.entries(props.ruleGraph.nodes).map(([id, node]) => {
           const dragging = draggingID === id;
+          const overlapping =
+            !dragging && nodesOverlappingDraggingNode.indexOf(id) !== -1;
           return (
             <g
               key={id}
@@ -73,7 +85,11 @@ export function RuleGraphEditor(props: {
               style={{ cursor: dragging ? "grabbing" : "grab" }}
               onMouseDown={() => setDraggingID(id)}
             >
-              <NodeDesc nodeDesc={node.desc} dragging={dragging} />
+              <NodeDesc
+                nodeDesc={node.desc}
+                dragging={dragging}
+                overlapping={overlapping}
+              />
             </g>
           );
         })}
@@ -82,13 +98,17 @@ export function RuleGraphEditor(props: {
   );
 }
 
-function NodeDesc(props: { nodeDesc: NodeDesc; dragging: boolean }) {
+function NodeDesc(props: {
+  nodeDesc: NodeDesc;
+  dragging: boolean;
+  overlapping: boolean;
+}) {
   switch (props.nodeDesc.type) {
     case "JoinVar":
       return (
         <circle
           r={JOIN_VAR_NODE_RADIUS}
-          fill={props.dragging ? "red" : "blue"}
+          fill={props.dragging ? "red" : props.overlapping ? "orange" : "blue"}
         />
       );
     case "Relation": {
