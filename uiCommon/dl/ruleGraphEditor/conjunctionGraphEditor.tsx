f3447d08ccda7716624acda4f0cb96a7506fa71e
Pete Vilter <pete.vilter@gmail.com>
2022-09-16 00:23:29 -0400

    start ripping out force-directed layout

--- uiCommon/dl/ruleGraphEditor/conjunctionGraphEditor.tsx
+++ uiCommon/dl/ruleGraphEditor/conjunctionGraphEditor.tsx
@@ -107,89 +107,6 @@ export function ConjunctionGraphEditor(props: {
   });
   console.log("joinVarDesiredPositions", joinVardesiredPositions);
 
-  useEffect(() => {
-    const simulation = forceSimulation<D3Point>();
-
-    const d3NodesByID: {
-      [id: string]: D3Point;
-    } = {};
-    Object.entries(graph.nodes).forEach(([id, node]) => {
-      const d3Node: D3Point = { ...node.pos, id };
-      if (dragState && dragState.nodeID === id) {
-        d3Node.fx = dragState.position.x;
-        d3Node.fy = dragState.position.y;
-      }
-      if (node.desc.type === "Relation") {
-        if (node.desc.isHead) {
-          d3Node.fx = DEFAULT_POINT.x;
-          d3Node.fy = 20;
-        } else {
-          d3Node.fx = node.pos.x;
-          d3Node.fy = node.pos.y;
-        }
-      }
-      d3NodesByID[id] = d3Node;
-    });
-
-    // update state on every frame
-    simulation.on("tick", () => {
-      setGraph({
-        ...graph,
-        nodes: mapObj(graph.nodes, (id, node) => {
-          const d3Node = d3NodesByID[id];
-          return {
-            ...node,
-            pos: { x: Math.round(d3Node.x), y: Math.round(d3Node.y) },
-          };
-        }),
-      });
-    });
-
-    const linksForce = forceLink();
-    linksForce
-      .links(
-        graph.edges.map((edge) => ({
-          source: d3NodesByID[edge.fromID],
-          target: d3NodesByID[edge.toID],
-        }))
-      )
-      .distance(100)
-      .strength(5);
-
-    // simulation.force("links", linksForce);
-    simulation.force(
-      "x",
-      forceX((node) => {
-        const desired = joinVardesiredPositions[node.id];
-        if (desired) {
-          return desired.x;
-        }
-        return DEFAULT_POINT.x;
-      })
-    );
-    simulation.force(
-      "y",
-      forceY((node) => {
-        const desired = joinVardesiredPositions[node.id];
-        if (desired) {
-          return desired.y;
-        }
-        return DEFAULT_POINT.y;
-      })
-    );
-    // simulation.force("charge", forceManyBody().strength(-50));
-
-    // copy nodes into simulation
-    simulation.nodes(Object.values(d3NodesByID));
-    // slow down with a small alpha
-    simulation.alpha(0.1).restart();
-
-    // stop simulation on unmount
-    return () => {
-      simulation.stop();
-    };
-  }, [props.rule, dragState]);
-
   return (
     <>
       <div>
@@ -449,6 +366,7 @@ function reducer(action: NodeAction, context: Context, state: State): State {
       };
     }
     case "StartDrag": {
+      const node = state.graph.nodes[action.id];
       return {
         ...state,
         dragState: {
