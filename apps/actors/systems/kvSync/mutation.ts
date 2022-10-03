f5fd7ec6cdde5fccc2cb6525fd5a25c1176a1ba3
Pete Vilter <pete.vilter@gmail.com>
2022-10-02 22:47:16 -0400

    build out mutations

--- apps/actors/systems/kvSync/mutation.ts
+++ apps/actors/systems/kvSync/mutation.ts
@@ -5,13 +5,17 @@ type Expr =
   | { type: "Do"; ops: Expr[] }
   | { type: "Read"; key: Expr }
   | { type: "Write"; key: Expr; val: Expr }
-  | { type: "Let"; varName: string; val: Expr }
+  | { type: "Let"; bindings: { varName: string; val: Expr }[]; body: Expr }
   | {
       type: "If";
       cond: Expr;
       ifTrue: Expr;
       ifFalse: Expr;
-    };
+    }
+  | { type: "Abort"; reason: Expr }
+  | { type: "Var"; name: string }
+  | { type: "StringLit"; val: string }
+  | { type: "Apply"; name: string; args: Expr[] };
 
 export function lambda(args: string[], body: Expr): Expr {
   return { type: "Lambda", args, body };
@@ -21,8 +25,11 @@ export function ifExpr(cond: Expr, ifTrue: Expr, ifFalse: Expr): Expr {
   return { type: "If", cond, ifTrue, ifFalse };
 }
 
-export function letExpr(varName: string, val: Expr): Expr {
-  return { type: "Let", varName, val };
+export function letExpr(
+  bindings: { varName: string; val: Expr }[],
+  body: Expr
+): Expr {
+  return { type: "Let", bindings, body };
 }
 
 export function read(key: Expr): Expr {
@@ -35,4 +42,20 @@ export function write(key: Expr, val: Expr): Expr {
 
 export function doExpr(ops: Expr[]): Expr {
   return { type: "Do", ops };
+}
+
+export function varr(name: string): Expr {
+  return { type: "Var", name };
+}
+
+export function apply(name: string, args: Expr[]): Expr {
+  return { type: "Apply", name, args };
+}
+
+export function abort(reason: Expr): Expr {
+  return { type: "Abort", reason };
+}
+
+export function str(val: string): Expr {
+  return { type: "StringLit", val };
 }
