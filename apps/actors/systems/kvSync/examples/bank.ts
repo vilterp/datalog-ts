4c475e242275119dff74667abb7c45e333ca870a
Pete Vilter <pete.vilter@gmail.com>
2022-10-02 22:47:00 -0400

    write out bank examples

--- apps/actors/systems/kvSync/examples/bank.ts
+++ apps/actors/systems/kvSync/examples/bank.ts
@@ -1,5 +1,59 @@
+import {
+  apply,
+  read,
+  varr,
+  letExpr,
+  ifExpr,
+  lambda,
+  abort,
+  str,
+  write,
+  doExpr,
+} from "../mutation";
 import { MutationDefns } from "../types";
 
 export const bankMutations: MutationDefns = {
-  moveMoney: XXXX,
+  deposit: lambda(
+    ["toAccount", "amount"],
+    letExpr(
+      [{ varName: "balanceBefore", val: read(varr("toAccount")) }],
+      write(
+        varr("toAccount"),
+        apply("+", [varr("balanceBefore"), varr("amount")])
+      )
+    )
+  ),
+  withdraw: lambda(
+    ["fromAccount", "amount"],
+    letExpr(
+      [{ varName: "balanceBefore", val: read(varr("fromAccount")) }],
+      write(
+        varr("fromAccount"),
+        apply("-", [varr("balanceBefore"), varr("amount")])
+      )
+    )
+  ),
+  move: lambda(
+    ["fromAccount", "toAccount", "amount"],
+    letExpr(
+      [
+        { varName: "fromBalance", val: read(varr("fromAccount")) },
+        { varName: "toBalance", val: read(varr("toAccount")) },
+      ],
+      ifExpr(
+        apply(">", [varr("amount"), varr("fromBalance")]),
+        abort(str("balance not high enough")),
+        doExpr([
+          write(
+            varr("fromAccount"),
+            apply("-", [varr("fromBalance"), varr("amount")])
+          ),
+          write(
+            varr("toAccount"),
+            apply("+", [varr("toBalance"), varr("amount")])
+          ),
+        ])
+      )
+    )
+  ),
 };
