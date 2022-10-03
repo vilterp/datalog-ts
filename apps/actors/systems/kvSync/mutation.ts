da3afc9bdb37973f34506b25134f5c5b7aedaa58
Pete Vilter <pete.vilter@gmail.com>
2022-10-02 22:32:19 -0400

    convenience constructors

--- apps/actors/systems/kvSync/mutation.ts
+++ apps/actors/systems/kvSync/mutation.ts
@@ -12,3 +12,27 @@ type Expr =
       ifTrue: Expr;
       ifFalse: Expr;
     };
+
+export function lambda(args: string[], body: Expr): Expr {
+  return { type: "Lambda", args, body };
+}
+
+export function ifExpr(cond: Expr, ifTrue: Expr, ifFalse: Expr): Expr {
+  return { type: "If", cond, ifTrue, ifFalse };
+}
+
+export function letExpr(varName: string, val: Expr): Expr {
+  return { type: "Let", varName, val };
+}
+
+export function read(key: Expr): Expr {
+  return { type: "Read", key };
+}
+
+export function write(key: Expr, val: Expr): Expr {
+  return { type: "Write", key, val };
+}
+
+export function doExpr(ops: Expr[]): Expr {
+  return { type: "Do", ops };
+}
