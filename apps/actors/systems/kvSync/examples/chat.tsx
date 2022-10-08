26bdb5ef2236be91a277f1cb6b1493d3e31e547e
Pete Vilter <pete.vilter@gmail.com>
2022-10-08 10:23:18 -0400

    toggle between threads

--- apps/actors/systems/kvSync/examples/chat.tsx
+++ apps/actors/systems/kvSync/examples/chat.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { UIProps } from "../../../types";
 import { ClientState, TransactionState } from "../client";
 import { MutationDefns, UserInput } from "../types";
 import { TxnState } from "./common";
 import { KVApp } from "./types";
 
-function ChatUI(props: UIProps<ClientState, UserInput>) {
-  const threads = ["foo", "bar"];
-  const messages: {
-    id: number;
-    sender: string;
-    message: string;
-    state: TransactionState;
-  }[] = [
+type Message = {
+  id: number;
+  sender: string;
+  message: string;
+  state: TransactionState;
+};
+
+const EXAMPLE_MESSAGES: {
+  [room: string]: Message[];
+} = {
+  foo: [
     {
       id: 1,
       message: "hello world",
       sender: "Pete",
       state: { type: "Committed", serverTimestamp: 2 },
     },
-  ];
+  ],
+  bar: [
+    {
+      id: 1,
+      message: "goodbye world",
+      sender: "RePete",
+      state: { type: "Committed", serverTimestamp: 2 },
+    },
+  ],
+};
+
+const EXAMPLE_THREADS = ["foo", "bar"];
+
+function ChatUI(props: UIProps<ClientState, UserInput>) {
+  const [curThread, setCurThread] = useState("foo");
+  const messages = EXAMPLE_MESSAGES[curThread];
+
   return (
     <div>
       <h3>Chat</h3>
@@ -28,8 +47,17 @@ function ChatUI(props: UIProps<ClientState, UserInput>) {
           <tr>
             <td>
               <ul>
-                {threads.map((thread) => (
-                  <li key={thread}>{thread}</li>
+                {EXAMPLE_THREADS.map((thread) => (
+                  <li
+                    key={thread}
+                    onClick={() => setCurThread(thread)}
+                    style={{
+                      cursor: "pointer",
+                      fontWeight: thread == curThread ? "bold" : "normal",
+                    }}
+                  >
+                    {thread}
+                  </li>
                 ))}
               </ul>
             </td>
