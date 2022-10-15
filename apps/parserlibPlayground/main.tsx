b595250aa1006db998b76eaac756762551d49fde
Pete Vilter <pete.vilter@gmail.com>
2022-10-15 12:37:36 -0400

    more parserlib playground

--- apps/parserlibPlayground/main.tsx
+++ apps/parserlibPlayground/main.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import ReactJson from "react-json-view";
+import { GRAMMAR } from "../../languageWorkbench/languages/grammar/parser";
+import { parse, TraceTree } from "../../languageWorkbench/parserlib/parser";
 
 function Main() {
-  return <p>Hello world</p>;
+  const [text, setText] = useState("");
+  const [tree, setTree] = useState<TraceTree>({
+    type: "SucceedTrace",
+    width: 0,
+    error: null,
+  });
+
+  return (
+    <>
+      <h1>Parserlib Playground</h1>
+
+      <textarea
+        value={text}
+        onChange={(evt) => {
+          setText(evt.target.value);
+          setTree(parse(GRAMMAR, "main", text));
+        }}
+      />
+
+      <ReactJson
+        src={tree}
+        displayDataTypes={false}
+        displayObjectSize={false}
+      />
+    </>
+  );
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"));
