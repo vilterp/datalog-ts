2148687264f7d48d00b7148e1bd3579282274181
Pete Vilter <pete.vilter@gmail.com>
2022-11-24 11:00:25 -0600

    reduce benchmark runs

--- languageWorkbench/benchmarks.ts
+++ languageWorkbench/benchmarks.ts
@@ -2,7 +2,7 @@ import { BenchmarkSpec, runDDBenchmark } from "../util/testBench/benchmark";
 import { testLangQuery } from "./ddTests";
 
 export const lwbBenchmarks: BenchmarkSpec[] = [
-  { lang: "fp", reps: 30 },
+  { lang: "fp", reps: 10 },
   { lang: "dl", reps: 10 },
 ].map(({ lang, reps }) => ({
   name: lang,
