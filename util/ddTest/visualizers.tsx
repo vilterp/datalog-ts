e0c552a635d78561e7f8c262fd12fe200bc34a63
Pete Vilter <pete.vilter@gmail.com>
2022-11-27 16:21:59 -0500

    try table view

--- util/ddTest/visualizers.tsx
+++ util/ddTest/visualizers.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactJson from "react-json-view";
+import { AbstractInterpreter } from "../../core/abstractInterpreter";
+import { nullLoader } from "../../core/loaders";
+import { SimpleInterpreter } from "../../core/simple/interpreter";
+import { noHighlightProps } from "../../uiCommon/dl/term";
+import { ResultsTable } from "../../uiCommon/explorer/resultsTable";
+import { emptyTableCollapseState } from "../../uiCommon/explorer/types";
 import { TestOutput } from "./types";
 
 export type SuiteSpec = {
@@ -12,7 +18,7 @@ export type SuiteSpec = {
 export const VISUALIZERS = {
   "text/plain": (text) => <pre style={{ margin: 0 }}>{text}</pre>,
   // TODO: syntax highlight using TermView?
-  "application/datalog": (text) => <pre style={{ margin: 0 }}>{text}</pre>,
+  "application/datalog": (text) => <DLView text={text} />,
   // TODO: reinstate an actual graphviz viewer
   "application/graphviz": (text) => <pre style={{ margin: 0 }}>{text}</pre>,
   "application/json": (text) => (
@@ -25,3 +31,27 @@ export const VISUALIZERS = {
     />
   ),
 };
+
+function DLView(props: { text: string }) {
+  let interp: AbstractInterpreter = new SimpleInterpreter(".", nullLoader);
+  interp = interp.evalStr(props.text)[1];
+
+  const [collapseState, setCollapseState] = useState(emptyTableCollapseState);
+
+  const tables = interp.getTables();
+  if (tables.length === 0) {
+    return <em>No tables</em>;
+  }
+
+  const relation = interp.getTables()[0];
+
+  return (
+    <ResultsTable
+      relation={{ type: "Table", name: relation }}
+      collapseState={collapseState}
+      setCollapseState={setCollapseState}
+      highlight={noHighlightProps}
+      results={interp.queryStr(`${relation}{}`)}
+    />
+  );
+}
