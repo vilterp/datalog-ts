50c849aa40600715837edfa56645fe8a0f8852b2
Pete Vilter <pete.vilter@gmail.com>
2022-12-02 23:21:27 -0500

    hook up benchmark

--- allBenchmarks.ts
+++ allBenchmarks.ts
@@ -1,10 +1,14 @@
 import { BenchmarkSpec, runAll } from "./util/testBench/benchmark";
 import { parserBenchmarks } from "./languageWorkbench/parserlib/benchmarks";
-import { lwbBenchmarks } from "./languageWorkbench/benchmarks";
+import {
+  lwbBenchmarks,
+  nativeDLBenchmark,
+} from "./languageWorkbench/benchmarks";
 
 const allBenchmarks: { [name: string]: BenchmarkSpec[] } = {
   parserBenchmarks,
   lwbBenchmarks,
+  nativeDLBenchmarks: [nativeDLBenchmark],
 };
 
 // just cuz we don't have top-level await...
