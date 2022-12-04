ce4907fb412f8187e0231af5e470e2e16276e9ed
Pete Vilter <pete.vilter@gmail.com>
2022-12-04 10:30:50 -0500

    benchmarks with time budget

--- languageWorkbench/benchmarks.ts
+++ languageWorkbench/benchmarks.ts
@@ -1,16 +1,12 @@
 import { BenchmarkSpec, runDDBenchmark } from "../util/testBench/benchmark";
 import { testLangQuery } from "./ddTests";
 
-export const lwbBenchmarks: BenchmarkSpec[] = [
-  { lang: "fp", reps: 10 },
-  { lang: "dl", reps: 10 },
-].map(({ lang, reps }) => ({
+export const lwbBenchmarks: BenchmarkSpec[] = ["fp", "dl"].map((lang) => ({
   name: lang,
   async run() {
     return runDDBenchmark(
       `languageWorkbench/languages/${lang}/${lang}.dd.txt`,
-      testLangQuery,
-      reps
+      testLangQuery
     );
   },
 }));
