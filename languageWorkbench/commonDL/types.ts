99a0bfd26e7aeafa02fca857328fdd2b105d663f
Pete Vilter <pete.vilter@gmail.com>
2022-12-03 19:06:37 -0500

    factor out IDE & types

---
+++ languageWorkbench/commonDL/types.ts
@@ -0,0 +1,51 @@
+import { NodesByRule } from "../parserlib/flattenByRule";
+import { Span } from "../parserlib/types";
+
+export type LangImpl = {
+  scopeDefn: (db: NodesByRule) => Generator<Defn>;
+  scopeVar: (db: NodesByRule) => Generator<Var>;
+  scopePlaceholder: (db: NodesByRule) => Generator<Placeholder>;
+  scopeParent: (
+    db: NodesByRule
+  ) => Generator<{ scopeID: string; parentID: string }>;
+  highlightMapping: { [ruleName: string]: TokenType };
+};
+
+export type Defn = {
+  scopeID: string;
+  span: Span;
+  name: string;
+  kind: string;
+};
+
+export type Var = {
+  scopeID: string;
+  name: string;
+  span: Span;
+  kind: string;
+};
+
+export type Suggestion = {
+  scopeID: string;
+  name: string;
+  span: Span;
+  type: string;
+};
+
+export type Placeholder = {
+  scopeID: string;
+  span: Span;
+  kind: string;
+};
+
+export type TokenType =
+  | "ident"
+  | "typeParameter"
+  | "number"
+  | "number"
+  | "string"
+  | "comment"
+  | "keyword"
+  | "string";
+
+export type SemanticToken = { type: TokenType; span: Span };
