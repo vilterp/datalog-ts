38b3180011bff409cf146d5e61112b2592e1c642
Pete Vilter <pete.vilter@gmail.com>
2022-12-28 00:13:02 -0500

    embed a full explorer for viewing datalog

--- apps/ddTestViewer/visualizers.tsx
+++ apps/ddTestViewer/visualizers.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactJson from "react-json-view";
 import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { nullLoader } from "../../core/loaders";
 import { SimpleInterpreter } from "../../core/simple/interpreter";
-import { noHighlightProps } from "../../uiCommon/dl/term";
-import { ResultsTable } from "../../uiCommon/explorer/resultsTable";
-import { emptyTableCollapseState } from "../../uiCommon/explorer/types";
+import { Explorer } from "../../uiCommon/explorer";
 import { TestOutput } from "../../util/ddTest/types";
 
 export type SuiteSpec = {
@@ -36,25 +34,9 @@ function DLView(props: { text: string }) {
   let interp: AbstractInterpreter = new SimpleInterpreter(".", nullLoader);
   interp = interp.evalStr(props.text)[1];
 
-  const [collapseState, setCollapseState] = useState(emptyTableCollapseState);
-
-  const tables = interp.getTables();
-  if (tables.length === 0) {
-    return <em>No tables</em>;
-  }
-
-  const relation = interp.getTables()[0];
-
   return (
     <>
-      <pre style={{ fontWeight: "bold" }}>{relation}</pre>
-      <ResultsTable
-        relation={{ type: "Table", name: relation }}
-        collapseState={collapseState}
-        setCollapseState={setCollapseState}
-        highlight={noHighlightProps}
-        results={interp.queryStr(`${relation}{}`)}
-      />
+      <Explorer interp={interp} />
     </>
   );
 }
