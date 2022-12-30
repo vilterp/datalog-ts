7199208640e82f672271b6c2219b469a48d91446
Pete Vilter <pete.vilter@gmail.com>
2022-12-30 00:07:10 -0500

    get rid of errors

--- core/incremental/indexedMultiSet.ts
+++ core/incremental/indexedMultiSet.ts
@@ -2,34 +2,40 @@ import { DefaultDict } from "../../util/defaultDict";
 import { clamp } from "../../util/util";
 
 export function emptyIndexedCollection<T>(
-  stringify: (t: T) => string
+  stringify: (t: T) => Key
 ): IndexedMultiSet<T> {
   return new IndexedMultiSet(
-    new DefaultDict<string, ItemAndMult<T>>(() => XXX),
-    new DefaultDict<string, Index<T>>(() => XXX),
+    new DefaultDict<Key, ItemAndMult<T>>(() => {
+      throw new Error("bloop");
+    }),
+    new DefaultDict<string, Index<T>>(() => {
+      throw new Error("bloop");
+    }),
     stringify
   );
 }
 
 type ItemAndMult<T> = { item: T; mult: number };
 
+type Key = string[];
+
 type Index<T> = {
-  getKey: (t: T) => string[];
-  items: DefaultDict<string[], Set<string>>;
+  getKey: (t: T) => Key;
+  items: DefaultDict<Key, Set<Key>>;
 };
 
 const MULT_RANGE: [number, number] = [-1, 1];
 
 export class IndexedMultiSet<T> {
-  private readonly allRecords: DefaultDict<string, ItemAndMult<T>>;
+  private readonly allRecords: DefaultDict<Key, ItemAndMult<T>>;
   private readonly indexes: DefaultDict<string, Index<T>>;
-  private readonly stringify: (t: T) => string;
+  private readonly stringify: (t: T) => Key;
   readonly size: number;
 
   constructor(
-    allRecords: DefaultDict<string, ItemAndMult<T>>,
+    allRecords: DefaultDict<Key, ItemAndMult<T>>,
     indexes: DefaultDict<string, Index<T>>,
-    stringify: (t: T) => string
+    stringify: (t: T) => Key
   ) {
     this.allRecords = allRecords;
     this.indexes = indexes;
@@ -46,9 +52,7 @@ export class IndexedMultiSet<T> {
   }
 
   createIndex(name: string, getKey: (t: T) => string[]): IndexedMultiSet<T> {
-    const indexItems = new DefaultDict<string[], Set<string>>(
-      () => new Set<string>()
-    );
+    const indexItems = new DefaultDict<Key, Set<Key>>(() => new Set<Key>());
     for (const [key, val] of this.allRecords.entries()) {
       const keyForItem = getKey(val.item);
       const itemsForKey = indexItems.get(keyForItem);
@@ -61,23 +65,22 @@ export class IndexedMultiSet<T> {
   update(item: T, multiplicityDelta: number): IndexedMultiSet<T> {
     // TODO: remove item if newMult is 0?
     const key = this.stringify(item);
-    const curMult = (this.allRecords.get(key) || { item, mult: 0 }).mult;
+    const curMult = this.allRecords.getWithDefault(key, { item, mult: 0 }).mult;
     const newMult = clamp(curMult + multiplicityDelta, MULT_RANGE);
     for (const index of this.indexes.values()) {
       const idxKey = index.getKey(item);
-      const items = index.items.get(idxKey) || new Set();
+      const items = index.items.get(idxKey);
       items.add(key);
     }
     return this;
   }
 
-  getByIndex(indexName: string, key: string[]): Map<T, number> {
-    const keySet: Set<string> = this.indexes.get(indexName).items.get(key);
-    return new Map(
-      keySet.map((key) => {
-        const { item, mult } = this.allRecords.get(key);
-        return [item, mult];
-      })
-    );
+  getByIndex(indexName: string, key: string[]): [Key, ItemAndMult<T>][] {
+    const keySet: Set<Key> = this.indexes.get(indexName).items.get(key);
+    const out: [Key, ItemAndMult<T>][] = [];
+    for (const key of keySet) {
+      out.push([key, this.allRecords.get(key)]);
+    }
+    return out;
   }
 }
