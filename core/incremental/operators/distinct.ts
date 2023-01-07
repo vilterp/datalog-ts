50192239bda21efcee11553a6523945e00ccb4ad
Pete Vilter <pete.vilter@gmail.com>
2023-01-06 20:14:36 -0500

    fix distinct

--- core/incremental/operators/distinct.ts
+++ core/incremental/operators/distinct.ts
@@ -8,13 +8,14 @@ export function processDistinct(
   if (data.type === "Record") {
     throw new Error("distinct nodes shouldn't get Record messages");
   }
-  const newDesc: DistinctDesc = {
-    ...desc,
-    state: desc.state.update(data.bindings.bindings, payload.multiplicity),
-  };
   const curMult = desc.state.get(data.bindings.bindings);
   const newMult = curMult + payload.multiplicity;
   const multDiff =
     curMult < 1 && newMult >= 1 ? 1 : curMult >= 1 && newMult < 1 ? -1 : 0;
+  // TODO: this is confusing because desc.state is the same object
+  const newDesc: DistinctDesc = {
+    ...desc,
+    state: desc.state.update(data.bindings.bindings, payload.multiplicity),
+  };
   return [newDesc, [{ ...payload, multiplicity: multDiff }]];
 }
