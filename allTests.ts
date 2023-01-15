cf7e2d0ee346e5e91e9c4eed8a2bc3a87847f601
Pete Vilter <pete.vilter@gmail.com>
2023-01-15 03:44:05 -0500

    uncomment tests

--- allTests.ts
+++ allTests.ts
@@ -22,22 +22,22 @@ const writeResults = flags.has("--write-results");
 const stayAlive = flags.has("--stay-alive");
 
 const suites: { [name: string]: Suite } = {
-  // unifyTests,
-  // parserTests: parserTests(writeResults),
-  // // TODO: it does seem kind of bad to have two test suites that use the same set of dd files
-  // coreTestsSimple: coreTestsSimple(writeResults),
-  // coreTestsIncremental: coreTestsIncremental(writeResults),
-  // coreTestsCommon: coreTestsCommon(writeResults),
-  // joinOrderTests: joinOrderTests(writeResults),
-  // incrTests: incrTests(writeResults),
-  // prettyPrintTests,
-  // treeTests,
+  unifyTests,
+  parserTests: parserTests(writeResults),
+  // TODO: it does seem kind of bad to have two test suites that use the same set of dd files
+  coreTestsSimple: coreTestsSimple(writeResults),
+  coreTestsIncremental: coreTestsIncremental(writeResults),
+  coreTestsCommon: coreTestsCommon(writeResults),
+  joinOrderTests: joinOrderTests(writeResults),
+  incrTests: incrTests(writeResults),
+  prettyPrintTests,
+  treeTests,
   parserlibTests: parserlibTests(writeResults),
-  // sourcePositionsTests,
-  // kvSync: kvSyncTests(writeResults),
-  // raceDetector: raceDetectorTests(writeResults),
-  // lwbTestsSimple: lwbTestsSimple(writeResults),
-  // lwbTestsIncr: lwbTestsIncr(writeResults),
+  sourcePositionsTests,
+  kvSync: kvSyncTests(writeResults),
+  raceDetector: raceDetectorTests(writeResults),
+  lwbTestsSimple: lwbTestsSimple(writeResults),
+  lwbTestsIncr: lwbTestsIncr(writeResults),
 };
 
 try {
