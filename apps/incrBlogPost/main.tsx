6e09af9f096f661820fcf71ff1fa608fe4e25c33
Pete Vilter <pete.vilter@gmail.com>
2023-02-25 14:53:25 -0500

    fix up reducer

--- apps/incrBlogPost/main.tsx
+++ apps/incrBlogPost/main.tsx
@@ -1,29 +1,40 @@
+import { IncrementFactor } from "aws-sdk/clients/iot";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { IncrementalInterpreter } from "../../core/incremental/interpreter";
 import { nullLoader } from "../../core/loaders";
 import { ppb } from "../../core/pretty";
-import { int, rec, str } from "../../core/types";
+import { int, rec, Statement, str } from "../../core/types";
 import { Explorer } from "../../uiCommon/explorer";
 import { EditableGraph } from "./editableGraph";
 
-let exampleInterp = new IncrementalInterpreter(".", nullLoader);
+let initialExampleInterp = new IncrementalInterpreter(".", nullLoader);
 // TODO: load these from
-exampleInterp = exampleInterp.evalStr(
+initialExampleInterp = initialExampleInterp.evalStr(
   ".table node"
 )[1] as IncrementalInterpreter;
-exampleInterp = exampleInterp.evalStr(
+initialExampleInterp = initialExampleInterp.evalStr(
   ".table edge"
 )[1] as IncrementalInterpreter;
 // TODO: install transitive closure
 
 // TODO: extract graph from example interp; insert into history interp
-let historyInterp = new IncrementalInterpreter(".", nullLoader);
-historyInterp = historyInterp.evalStr(
+let initialHistoryInterp = new IncrementalInterpreter(".", nullLoader);
+initialHistoryInterp = initialHistoryInterp.evalStr(
   ".table step"
 )[1] as IncrementalInterpreter;
 
 function Main() {
+  const [exampleInterp, setExampleInterp] =
+    React.useState(initialExampleInterp);
+  const [historyInterp, dispatchHistoryInterp] = React.useReducer(
+    (st: IncrementalInterpreter, action: Statement): IncrementalInterpreter =>
+      st.evalStmt(action)[1] as IncrementalInterpreter,
+    initialHistoryInterp
+  );
+
+  console.log({ exampleInterp, historyInterp });
+
   return (
     <div>
       <h2>Incremental Datalog</h2>
@@ -36,16 +47,19 @@ function Main() {
         </li>
       </ul>
       <p>Take this graph problem as an example:</p>
+      <Explorer interp={exampleInterp} />
       <EditableGraph
         interp={exampleInterp}
         runStmts={(stmts) => {
+          // TODO: introduce user step id
           stmts.forEach((stmt) => {
             const res = exampleInterp.processStmt(stmt);
+            setExampleInterp(res.newInterp as IncrementalInterpreter);
             if (res.output.type === "EmissionLog") {
               res.output.log.forEach((item) => {
                 item.output.forEach((logItem) => {
                   const data = logItem.data;
-                  historyInterp.evalStmt({
+                  dispatchHistoryInterp({
                     type: "Fact",
                     record: rec("step", {
                       fromID: str(item.fromID),
@@ -66,6 +80,7 @@ function Main() {
         You can add and remove nodes and edges, and see the transitive closure
         edges update.
       </p>
+      <p>A log of what you've done can be seen in this explorer:</p>
       <Explorer interp={historyInterp} />
     </div>
   );
