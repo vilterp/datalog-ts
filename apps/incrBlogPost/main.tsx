913e5a346f253a867f16f53c4f022cec9742be68
Pete Vilter <pete.vilter@gmail.com>
2023-02-25 12:59:52 -0500

    start incr blog post

---
+++ apps/incrBlogPost/main.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function Main() {
+  return <p>Hello world</p>;
+}
+
+ReactDOM.render(<Main />, document.getElementById("main"));
