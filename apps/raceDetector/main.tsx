f48b2d8460cb73a902429aed4e32220af8edf87a
Pete Vilter <pete.vilter@gmail.com>
2023-06-09 20:35:27 -0500

    add lingo editor with basic blocks grammar

--- apps/raceDetector/main.tsx
+++ apps/raceDetector/main.tsx
@@ -6,6 +6,23 @@ import { Statement } from "../../core/types";
 import { Explorer } from "../../uiCommon/explorer";
 // @ts-ignore
 import execDL from "./execution.dl";
+import { LingoEditor } from "../../uiCommon/ide/editor";
+import { LANGUAGES } from "../../languageWorkbench/languages";
+import { initialEditorState } from "../../uiCommon/ide/types";
+
+const EXAMPLE = `foo {
+  a = 42;
+  b = "foo";
+  x = boop(a);
+  c = group(???);
+  goto ???;
+}
+
+bar {
+  y = boop;
+  goto foo;
+}
+`;
 
 const emptyInterp = new IncrementalInterpreter(".", nullLoader);
 const loadedInterp = emptyInterp.evalStr(execDL)[1];
@@ -16,10 +33,18 @@ function Main() {
       state.evalRawStmts(action)[1],
     loadedInterp
   );
+  const [editorState, setEditorState] = useState(initialEditorState(EXAMPLE));
 
   return (
     <>
       <h1>Race Detector</h1>
+
+      <LingoEditor
+        langSpec={LANGUAGES.basicBlocks}
+        editorState={editorState}
+        setEditorState={setEditorState}
+      />
+
       <Explorer
         interp={interp}
         runStatements={(stmts) => {
