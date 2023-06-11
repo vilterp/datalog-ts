389b145f040bbd9136160e42992f9a8eb2d0aba4
Pete Vilter <pete.vilter@gmail.com>
2023-06-11 14:46:06 -0400

    remove execution test
    not using it anymore

--- apps/raceDetector/ddTests.ts
+++ apps/raceDetector/ddTests.ts
@@ -1,26 +1,14 @@
 import { AbstractInterpreter } from "../../core/abstractInterpreter";
 import { fsLoader } from "../../core/fsLoader";
 import { IncrementalInterpreter } from "../../core/incremental/interpreter";
-import { int, rec } from "../../core/types";
 import { parseMain } from "../../languageWorkbench/languages/basicBlocks/parser";
 import { runDDTestAtPath } from "../../util/ddTest";
 import { datalogOut, TestOutput } from "../../util/ddTest/types";
 import { Suite } from "../../util/testBench/testing";
-import { range } from "../../util/util";
 import { compileBasicBlocks } from "./compiler";
 
 export function raceDetectorTests(writeResults: boolean): Suite {
   return [
-    // {
-    //   name: "execution",
-    //   test() {
-    //     runDDTestAtPath(
-    //       "apps/raceDetector/testdata/execution.dd.txt",
-    //       getResults,
-    //       writeResults
-    //     );
-    //   },
-    // },
     {
       name: "compiler",
       test() {
@@ -42,21 +30,6 @@ export function raceDetectorTests(writeResults: boolean): Suite {
       },
     },
   ];
-}
-
-function getResults(inputs: string[]): TestOutput[] {
-  let interp: AbstractInterpreter = new IncrementalInterpreter(
-    "apps/raceDetector",
-    fsLoader
-  );
-  interp = interp.doLoad("execution.dl");
-  const out: TestOutput[] = [];
-  inputs.forEach((input) => {
-    const [results, newInterp] = interp.evalStr(input);
-    interp = newInterp;
-    out.push(datalogOut(results.map((res) => res.term)));
-  });
-  return out;
 }
 
 function compilerTest(inputs: string[]): TestOutput[] {
