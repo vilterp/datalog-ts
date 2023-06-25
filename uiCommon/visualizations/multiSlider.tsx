505a26a8715f3cf26a1b3e2abb8c859c2330b8d1
Pete Vilter <pete.vilter@gmail.com>
2023-06-25 14:05:38 -0400

    multiSlider: show values

--- uiCommon/visualizations/multiSlider.tsx
+++ uiCommon/visualizations/multiSlider.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { VizArgs, VizTypeSpec } from "./typeSpec";
+import { Rec } from "../../core/types";
+import { ppt } from "../../core/pretty";
 
 export const multiSlider: VizTypeSpec = {
   name: "MultiSlider",
@@ -8,6 +10,29 @@ export const multiSlider: VizTypeSpec = {
   component: MultiSlider,
 };
 
+const BORDER_STYLE = "1px solid lightgrey";
+
 function MultiSlider(props: VizArgs) {
-  return <p>Hello world</p>;
+  const varRecs = props.interp.queryRec(props.spec.attrs.vars as Rec);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th style={{ borderBottom: BORDER_STYLE }}>Var</th>
+          <th style={{ borderBottom: BORDER_STYLE }}>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {varRecs.map((res) => (
+          <tr key={ppt(res.bindings.ID)}>
+            <td style={{ borderRight: BORDER_STYLE }}>
+              {ppt(res.bindings.ID)}
+            </td>
+            <td>{ppt(res.bindings.Val)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
