20f8405f13907fa717a3f8650dddc9bf2fba10e6
Pete Vilter <pete.vilter@gmail.com>
2023-06-28 00:57:46 -0400

    start implementing primitives

--- apps/executionVisualizer/interpreter.ts
+++ apps/executionVisualizer/interpreter.ts
@@ -33,7 +33,7 @@ type Timer = {
 
 type BlockReason = { type: "Timer"; id: string } | { type: "Lock"; id: string };
 
-type Value = string | number;
+type Value = string | number | boolean;
 
 export function initialState(instrs: BBMain): State {
   return {
@@ -93,14 +93,34 @@ function stepThread(state: State, threadID: string): State {
 
 function getRValue(rvalue: BBRvalue, threadState: ThreadState): Value {
   switch (rvalue.type) {
-    case "Call":
-      return XXX;
+    case "Call": {
+      const name = rvalue.ident.text;
+      const args = rvalue.params.ident.map(
+        (ident) => threadState.scope[ident.text]
+      );
+      if (name.startsWith("prim.")) {
+        return getPrimitiveResult(rvalue.ident.text, args);
+      } else if (name.startsWith("block.")) {
+        // this should be elsewhere
+      }
+    }
     case "EditorVar":
-      return XXX;
+      throw new Error("TODO: editor vars");
     case "Int":
       return parseInt(rvalue.text);
     case "String":
       // TODO: process escapes
       return rvalue.text;
+  }
+}
+
+function getPrimitiveResult(name: string, args: Value[]): Value {
+  switch (name) {
+    case "prim.add":
+      return (args[0] as number) + (args[1] as number);
+    case "prim.incr":
+      return (args[0] as number) + 1;
+    case "prim.lt":
+      return args[0] < args[1];
   }
 }
