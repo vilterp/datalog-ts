b98b33a3773a648f80ab8f4bdd010d8494b77dba
Pete Vilter <pete.vilter@gmail.com>
2023-07-14 17:42:43 +0300

    mouse utils

---
+++ util/diagrams/util.ts
@@ -0,0 +1,25 @@
+import { Ref, MouseEvent } from "react";
+import { Point } from "./types";
+
+export function mouseRelativeToElementTopLeft(
+  svgRef: Ref<SVGElement>,
+  evt: MouseEvent
+): Point {
+  // @ts-ignore
+  const svgRect = svgRef.current.getBoundingClientRect();
+  const x = evt.clientX - svgRect.left;
+  const y = evt.clientY - svgRect.top;
+  return { x, y };
+}
+
+export function mouseRelativeToElementCenter(
+  svgRef: Ref<SVGElement>,
+  evt: MouseEvent
+): Point {
+  // @ts-ignore
+  const svgRect = svgRef.current.getBoundingClientRect();
+  const center = rectCenter(svgRect);
+  const x = evt.clientX - center.x;
+  const y = evt.clientY - center.y;
+  return { x, y };
+}
