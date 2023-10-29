38a3240d202824b6ee8ecd8d70ea76d157cf67a6
Pete Vilter <pete.vilter@gmail.com>
2023-10-28 22:00:23 -0400

    pulling it together

--- uiCommon/visualizations/optimizer.tsx
+++ uiCommon/visualizations/optimizer.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import { VizArgs, VizTypeSpec } from "./typeSpec";
 import { SimplexSolver } from "../../core/opt/simplex";
+import { Int } from "../../core/types";
 
 export const optimizer: VizTypeSpec = {
   name: "Optimizer",
   description: "optimize stuff",
   component: (props: VizArgs) => {
+    const problemID = (props.spec.attrs.problem as Int).val;
+    const constraintCoefficients = props.interp.queryStr(
+      `coefficientValue{problem: ${problemID}}`
+    );
+    const objectiveCoefficients = props.interp.queryStr(
+      `objectiveCoefficient{problem: ${problemID}}`
+    );
+
+    console.log("opt", { constraintCoefficients, objectiveCoefficients });
+
     // Usage
     const A = [
       [2, 1],
