2c27ef58d21784dc7556953b612f0b32627ccefe
Pete Vilter <pete.vilter@gmail.com>
2023-12-29 17:16:44 -0500

    use the generated parser

--- apps/optPlayground/main.tsx
+++ apps/optPlayground/main.tsx
@@ -4,12 +4,16 @@ import { LingoEditor } from "../../uiCommon/ide/editor";
 import { LANGUAGES } from "../../languageWorkbench/languages";
 import { useJSONLocalStorage } from "../../uiCommon/generic/hooks";
 import { initialEditorState } from "../../uiCommon/ide/types";
+import { parseMain } from "../../languageWorkbench/languages/opt/parser";
+import ReactJson from "react-json-view";
 
 function Main() {
   const [editorState, setEditorState] = useJSONLocalStorage(
     "opt-playground-editor-state",
     initialEditorState(LANGUAGES.opt.example)
   );
+
+  const parsed = parseMain(editorState.source);
 
   return (
     <>
@@ -18,6 +22,12 @@ function Main() {
         langSpec={LANGUAGES.opt}
         editorState={editorState}
         setEditorState={setEditorState}
+      />
+      <ReactJson
+        src={parsed}
+        displayDataTypes={false}
+        displayObjectSize={false}
+        enableClipboard={false}
       />
     </>
   );
