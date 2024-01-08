ed1afed3fe0a3885e12fba4ee16f045e159ebd8b
Pete Vilter <pete.vilter@gmail.com>
2024-01-08 14:05:07 -0500

    use dl2 for dl2

--- languageWorkbench/languages/dl2/index.ts
+++ languageWorkbench/languages/dl2/index.ts
@@ -1,6 +1,6 @@
-import { LanguageSpec, dl } from "../../common/types";
+import { LanguageSpec, dl2 } from "../../common/types";
 // @ts-ignore
-import datalog from "./dl2.dl";
+import datalog from "./dl2.dl2";
 // @ts-ignore
 import grammar from "./dl2.grammar";
 // @ts-ignore
@@ -8,7 +8,7 @@ import example from "./example.txt";
 
 export const datalog2: LanguageSpec = {
   name: "datalog2",
-  logic: dl(datalog),
+  logic: dl2(datalog),
   grammar,
   example,
 };
