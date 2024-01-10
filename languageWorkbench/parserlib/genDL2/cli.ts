2f23fb64327343841854d2f6d5928e7107aa4181
Pete Vilter <pete.vilter@gmail.com>
2024-01-10 01:09:01 -0500

    Generate DL2 typings from grammars (#456)
    * script to generate DL2 typings
    
    * tables for dl2
    
    * add prefix
    
    * update table decls
    
    * fix

---
+++ languageWorkbench/parserlib/genDL2/cli.ts
@@ -0,0 +1,40 @@
+import * as fs from "fs";
+import { parseMain } from "../../languages/grammar/parser";
+import { parserGrammarToInternal } from "../translateAST";
+import { generateTableDecls } from "./generate";
+import { prettyPrintTableDecl } from "../../languages/dl2/compiler/pretty";
+
+type Args = {
+  grammarPath: string;
+  outputPath: string;
+};
+
+function main() {
+  // TODO: use some kind of actual option parser. optimist?
+  const args: Args = {
+    grammarPath: process.argv[2],
+    outputPath: process.argv[3],
+  };
+  console.log("generating with args", args);
+  generate(args);
+}
+
+function generate(args: Args) {
+  const grammarStr = fs.readFileSync(args.grammarPath, "utf-8");
+  const [grammarTree, errors] = parseMain(grammarStr);
+  if (errors.length > 0) {
+    console.log("failed to parse grammar:");
+    for (const error of errors) {
+      console.error(error);
+    }
+    process.exit(1);
+  }
+  const grammar = parserGrammarToInternal(grammarTree);
+  const tableDecls = generateTableDecls(grammar);
+  const code = Object.entries(tableDecls)
+    .map(([name, decl]) => prettyPrintTableDecl(name, decl))
+    .join("\n\n");
+  fs.writeFileSync(args.outputPath, code);
+}
+
+main();
---
+++ languageWorkbench/parserlib/genDL2/generate.ts
@@ -0,0 +1,63 @@
+import { flatMap, mapObj, objToPairs, pairsToObj } from "../../../util/util";
+import { TableDecl, TableMember } from "../../languages/dl2/compiler/types";
+import { Grammar, Rule } from "../types";
+
+const DEFAULT_LEAVES = new Set(["ident", "string", "int", "ws"]);
+
+const PREFIX = "ast.";
+
+export function generateTableDecls(
+  grammar: Grammar,
+  leaves = DEFAULT_LEAVES
+): {
+  [name: string]: TableDecl;
+} {
+  return pairsToObj(
+    objToPairs(grammar).map(([name, rule]) => {
+      const rawRefs = findRefs(rule);
+      const refs = leaves.has(name) ? [] : rawRefs;
+      return {
+        key: PREFIX + name,
+        value: {
+          members: pairsToObj([
+            { key: "id", value: { type: "Scalar" } },
+            { key: "parentID", value: { type: "Scalar" } },
+            { key: "text", value: { type: "Scalar" } },
+            { key: "span", value: { type: "Scalar" } },
+            ...refs.map((ref): { key: string; value: TableMember } => ({
+              key: ref.captureName,
+              value: {
+                type: "InRef",
+                table: PREFIX + ref.rule,
+                column: "parentID",
+              },
+            })),
+          ]),
+          facts: [],
+        },
+      };
+    })
+  );
+}
+
+type RuleRef = {
+  captureName: string;
+  rule: string;
+};
+
+function findRefs(rule: Rule): RuleRef[] {
+  switch (rule.type) {
+    case "Char":
+      return [];
+    case "Choice":
+      return flatMap(rule.choices, findRefs);
+    case "Ref":
+      return [{ rule: rule.rule, captureName: rule.captureName || rule.rule }];
+    case "RepSep":
+      return [...findRefs(rule.rep), ...findRefs(rule.sep)];
+    case "Sequence":
+      return flatMap(rule.items, findRefs);
+    case "Text":
+      return [];
+  }
+}
