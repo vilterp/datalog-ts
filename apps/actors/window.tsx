b8fd2244bb47c2bccc1b982e4102d755c57b7ac4
Pete Vilter <pete.vilter@gmail.com>
2024-06-01 14:52:51 -0400

    red circle close button

--- apps/actors/window.tsx
+++ apps/actors/window.tsx
@@ -10,7 +10,26 @@ export function Window(props: {
     <div style={{ border: "1px solid black", padding: "10px" }}>
       {/* top bar */}
       <div>
-        <button onClick={props.onClose}>Close</button>
+        <button
+          onClick={props.onClose}
+          style={{
+            backgroundColor: "red",
+            border: "none",
+            color: "white",
+            textAlign: "center",
+            textDecoration: "none",
+            display: "inline-block",
+            fontSize: "16px",
+            margin: "4px 2px",
+            cursor: "pointer",
+            borderRadius: "50%",
+            width: "20px",
+            height: "20px",
+            padding: "0",
+          }}
+        >
+          x
+        </button>
         {props.name}
       </div>
       {/* inner content */}
