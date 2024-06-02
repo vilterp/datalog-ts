9c0b5ab27356ec4543b7c033f6829af1af7a8a10
Pete Vilter <pete.vilter@gmail.com>
2024-06-02 17:28:03 -0400

    transaction list

--- apps/actors/systems/kvSync/examples/counter.tsx
+++ apps/actors/systems/kvSync/examples/counter.tsx
@@ -14,6 +14,7 @@ import { KVApp } from "./types";
 import { UIProps } from "../../../types";
 import { ClientState } from "../client";
 import { makeClient, useLiveQuery } from "../hooks";
+import { TransactionList } from "./common/txnList";
 
 function CounterUI(props: UIProps<ClientState, UserInput>) {
   const client = makeClient(props);
@@ -65,6 +66,7 @@ function CounterUI(props: UIProps<ClientState, UserInput>) {
       >
         +
       </button>
+      <TransactionList client={client} />
     </>
   );
 }
