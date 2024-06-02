d5a0b1e8a0590e229a3315b1132258278c46f245
Pete Vilter <pete.vilter@gmail.com>
2024-06-02 19:08:20 -0400

    collapsible inspector

--- apps/actors/systems/kvSync/examples/common/inspector.tsx
+++ apps/actors/systems/kvSync/examples/common/inspector.tsx
@@ -3,26 +3,33 @@ import { Client } from "../../hooks";
 import { Tabs } from "../../../../../../uiCommon/generic/tabs";
 import { TransactionList } from "./txnList";
 import { KVInspector } from "./kvInspector";
+import { Collapsible } from "../../../../../../uiCommon/generic/collapsible";
 
 export function Inspector(props: { client: Client }) {
   const [curTab, setTab] = useState("data");
 
   return (
-    <Tabs
-      curTabID={curTab}
-      setTabID={setTab}
-      tabs={[
-        {
-          id: "transactions",
-          name: "Transactions",
-          render: () => <TransactionList client={props.client} />,
-        },
-        {
-          id: "data",
-          name: "Data",
-          render: () => <KVInspector client={props.client} />,
-        },
-      ]}
+    <Collapsible
+      renderLabel={(collapsed) => <h4>{collapsed ? ">" : "v"} Inspector</h4>}
+      storageKey={props.client.state.id}
+      content={
+        <Tabs
+          curTabID={curTab}
+          setTabID={setTab}
+          tabs={[
+            {
+              id: "transactions",
+              name: "Transactions",
+              render: () => <TransactionList client={props.client} />,
+            },
+            {
+              id: "data",
+              name: "Data",
+              render: () => <KVInspector client={props.client} />,
+            },
+          ]}
+        />
+      }
     />
   );
 }
