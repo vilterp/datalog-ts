b22c11f344ef3ce663adf361ee8a43a6460a4180
Pete Vilter <pete.vilter@gmail.com>
2024-06-02 18:20:14 -0400

    fix react error

--- apps/actors/main.tsx
+++ apps/actors/main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import { Explorer } from "../../uiCommon/explorer";
 import ReactJson from "react-json-view";
 import { Json } from "../../util/json";
