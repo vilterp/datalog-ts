a20f5b66ed86a80a7acd6426b97122e263437453
Pete Vilter <pete.vilter@gmail.com>
2024-06-02 20:51:44 -0400

    don't use collapsible in inspector
    causing import issue

--- apps/actors/systems/kvSync/examples/common/inspector.tsx
+++ apps/actors/systems/kvSync/examples/common/inspector.tsx
@@ -3,33 +3,29 @@ import { Client } from "../../hooks";
 import { Tabs } from "../../../../../../uiCommon/generic/tabs";
 import { TransactionList } from "./txnList";
 import { KVInspector } from "./kvInspector";
-import { Collapsible } from "../../../../../../uiCommon/generic/collapsible";
 
 export function Inspector(props: { client: Client }) {
   const [curTab, setTab] = useState("data");
 
   return (
-    <Collapsible
-      renderLabel={(collapsed) => <h4>{collapsed ? ">" : "v"} Inspector</h4>}
-      storageKey={props.client.state.id}
-      content={
-        <Tabs
-          curTabID={curTab}
-          setTabID={setTab}
-          tabs={[
-            {
-              id: "transactions",
-              name: "Transactions",
-              render: () => <TransactionList client={props.client} />,
-            },
-            {
-              id: "data",
-              name: "Data",
-              render: () => <KVInspector client={props.client} />,
-            },
-          ]}
-        />
-      }
-    />
+    <>
+      <h4>Inspector</h4>
+      <Tabs
+        curTabID={curTab}
+        setTabID={setTab}
+        tabs={[
+          {
+            id: "transactions",
+            name: "Transactions",
+            render: () => <TransactionList client={props.client} />,
+          },
+          {
+            id: "data",
+            name: "Data",
+            render: () => <KVInspector client={props.client} />,
+          },
+        ]}
+      />
+    </>
   );
 }
