2c61b4668eaad129ece1a922e9387d496a769a3f
Pete Vilter <pete.vilter@gmail.com>
2024-06-04 13:53:01 -0400

    trace inspector

--- apps/actors/systems/kvSync/examples/common/txnList.tsx
+++ apps/actors/systems/kvSync/examples/common/txnList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Client } from "../../hooks";
 import { TxnState } from "./txnState";
 import { prettyPrintInvocation } from "./pretty";
@@ -6,26 +6,49 @@ import { TransactionRecord, TransactionState } from "../../client";
 import { Table } from "./table";
 
 export function TransactionList(props: { client: Client }) {
+  const [selectedTxnID, setSelectedTxnID] = useState<string | null>(null);
+
   return (
-    <Table<[string, TransactionRecord]>
-      columns={[
-        { name: "ID", render: ([id, txn]) => id },
-        {
-          name: "Invocation",
-          render: ([id, txn]) => (
-            <code>{prettyPrintInvocation(txn.invocation)}</code>
-          ),
-        },
-        {
-          name: "Status",
-          render: ([id, txn]) => <TxnState client={props.client} txnID={id} />,
-        },
-      ]}
-      data={Object.entries(props.client.state.transactions)
-        .sort(comparator)
-        .reverse()}
-      getKey={([id, txn]) => id}
-    />
+    <>
+      <Table<[string, TransactionRecord]>
+        columns={[
+          {
+            name: "ID",
+            render: ([id, txn]) => (
+              // TODO: row click
+              <span onClick={() => setSelectedTxnID(id)}>id</span>
+            ),
+          },
+          {
+            name: "Invocation",
+            render: ([id, txn]) => (
+              <code>{prettyPrintInvocation(txn.invocation)}</code>
+            ),
+          },
+          {
+            name: "Status",
+            render: ([id, txn]) => (
+              <TxnState client={props.client} txnID={id} />
+            ),
+          },
+        ]}
+        data={Object.entries(props.client.state.transactions)
+          .sort(comparator)
+          .reverse()}
+        getKey={([id, txn]) => id}
+      />
+      {selectedTxnID !== null ? (
+        <ol>
+          {props.client.state.transactions[selectedTxnID].clientTrace.map(
+            (op, idx) => (
+              <li key={idx}>
+                <code>{JSON.stringify(op)}</code>
+              </li>
+            )
+          )}
+        </ol>
+      ) : null}
+    </>
   );
 }
 
