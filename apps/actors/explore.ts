378e5dfe2378183ca5a89df45fff9c775997d2b6
Pete Vilter <pete.vilter@gmail.com>
2024-06-04 16:51:25 -0400

    more progress on the explore algorithm

--- apps/actors/explore.ts
+++ apps/actors/explore.ts
@@ -1,18 +1,18 @@
+import { Json } from "../../util/json";
 import { stepAll } from "./step";
-import { System, SystemInstance } from "./types";
+import { SystemInstance } from "./types";
 
 // TODO: generator of what? updates to a DB?
 
 type Frame<ActorState, Msg> = {
   state: SystemInstance<ActorState, Msg>;
   options: Generator;
-  messages: Msg[];
 };
 // TODO:
 // - process messages (maybe use `step`?)
 // - insert into DB
 // - stopping condition
-export function* explore<ActorState, Msg>(
+export function* explore<ActorState extends Json, Msg extends Json>(
   systemInstance: SystemInstance<ActorState, Msg>,
   stepLimit: number
 ): Generator<SystemInstance<ActorState, Msg>> {
@@ -25,7 +25,6 @@ export function* explore<ActorState, Msg>(
     {
       state: systemInstance,
       options: system.chooseNextMove(systemInstance),
-      messages: [],
     },
   ];
   let steps = 0;
@@ -45,7 +44,14 @@ export function* explore<ActorState, Msg>(
       continue;
     }
 
-    const nextState = stepAll(XXX, XXX, XXX);
+    const nextState = stepAll(frame.state.trace, frame.state.system.update, [
+      nextMsg,
+    ]);
+
+    stack.push({
+      options: system.chooseNextMove(nextState),
+      state: nextState,
+    });
 
     steps++;
   }
