c7e540e08c5a425c7ed0a315777f2fe072777c6d
Pete Vilter <pete.vilter@gmail.com>
2024-06-04 17:11:02 -0400

    choose function for counter

--- apps/actors/systems/kvSync/examples/types.ts
+++ apps/actors/systems/kvSync/examples/types.ts
@@ -1,10 +1,15 @@
 import { UIProps } from "../../../types";
 import { ClientState } from "../client";
-import { MutationDefns, UserInput } from "../types";
+import { KVData, MutationDefns, MutationInvocation, UserInput } from "../types";
+
+type ChooseFn = (clients: {
+  [id: string]: ClientState;
+}) => Generator<{ clientID: string; invocation: MutationInvocation }>;
 
 export type KVApp = {
   name: string;
   mutations: MutationDefns;
   initialKVPairs?: {};
   ui: (props: UIProps<ClientState, UserInput>) => React.ReactElement;
+  choose?: ChooseFn;
 };
