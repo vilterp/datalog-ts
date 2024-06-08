917330d2a3ba82ee37dbb4e5b2361c3c294d5b1f
Pete Vilter <pete.vilter@gmail.com>
2024-06-08 14:08:26 -0400

    make tabs look better

--- uiCommon/generic/tabs.tsx
+++ uiCommon/generic/tabs.tsx
@@ -8,26 +8,28 @@ export function Tabs(props: {
   return (
     <div>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        {props.tabs.map((tab, tabIdx) => (
-          <div
-            style={{
-              cursor: "pointer",
-              fontWeight: tab.id === props.curTabID ? "bold" : "normal",
-              backgroundColor:
-                tab.id === props.curTabID ? "lightgray" : "rgb(230, 230, 230)",
-              borderRight:
-                tabIdx === props.tabs.length - 1 ? null : "1px solid gray",
-              paddingLeft: 10,
-              paddingRight: 10,
-              paddingTop: 5,
-              paddingBottom: 5,
-            }}
-            onClick={() => props.setTabID(tab.id)}
-            key={tab.id}
-          >
-            {tab.name}
-          </div>
-        ))}
+        {props.tabs.map((tab, tabIdx) => {
+          const selected = tab.id === props.curTabID;
+          return (
+            <div
+              style={{
+                cursor: "pointer",
+                fontWeight: selected ? "bold" : "normal",
+                backgroundColor: selected ? null : "rgb(230, 230, 230)",
+                border: "1px solid grey",
+                borderBottom: selected ? "none" : "1px solid grey",
+                paddingLeft: 10,
+                paddingRight: 10,
+                paddingTop: 5,
+                paddingBottom: 5,
+              }}
+              onClick={() => props.setTabID(tab.id)}
+              key={tab.id}
+            >
+              {tab.name}
+            </div>
+          );
+        })}
       </div>
       <div>{props.tabs.find((tab) => tab.id === props.curTabID).render()}</div>
     </div>
