644002fdfdcd38e9d3a23c44b51594812b41fc0f
Pete Vilter <pete.vilter@gmail.com>
2024-06-08 14:21:59 -0400

    Make tabs look better (#475)
    * make tabs look better
    
    * don't need to bold anymore
    
    * fix borders
    
    * tabs padding

--- uiCommon/generic/tabs.tsx
+++ uiCommon/generic/tabs.tsx
@@ -4,32 +4,47 @@ export function Tabs(props: {
   tabs: { name: string; id: string; render: () => React.ReactElement }[];
   curTabID: string;
   setTabID: (id: string) => void;
+  innerPadding?: number;
 }) {
+  const border = "1px solid grey";
   return (
     <div>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        {props.tabs.map((tab, tabIdx) => (
-          <div
-            style={{
-              cursor: "pointer",
-              fontWeight: tab.id === props.curTabID ? "bold" : "normal",
-              backgroundColor:
-                tab.id === props.curTabID ? "lightgray" : "rgb(230, 230, 230)",
-              borderRight:
-                tabIdx === props.tabs.length - 1 ? null : "1px solid gray",
-              paddingLeft: 10,
-              paddingRight: 10,
-              paddingTop: 5,
-              paddingBottom: 5,
-            }}
-            onClick={() => props.setTabID(tab.id)}
-            key={tab.id}
-          >
-            {tab.name}
-          </div>
-        ))}
+        {/* left padding */}
+        <div style={{ borderBottom: "1px solid grey", width: 5 }} />
+        {/* tabs */}
+        {props.tabs.map((tab, tabIdx) => {
+          const selected = tab.id === props.curTabID;
+          return (
+            <div
+              style={{
+                cursor: "pointer",
+                backgroundColor: selected ? null : "rgb(230, 230, 230)",
+                // border
+                borderTop: border,
+                borderLeft: border,
+                borderRight: tabIdx === props.tabs.length - 1 ? border : "none",
+                borderBottom: selected ? "none" : border,
+                // padding
+                paddingLeft: 10,
+                paddingRight: 10,
+                paddingTop: 5,
+                paddingBottom: 5,
+              }}
+              onClick={() => props.setTabID(tab.id)}
+              key={tab.id}
+            >
+              {tab.name}
+            </div>
+          );
+        })}
+        {/* right padding */}
+        <div style={{ borderBottom: "1px solid grey", flexGrow: 1 }} />
       </div>
-      <div>{props.tabs.find((tab) => tab.id === props.curTabID).render()}</div>
+      {/* content */}
+      <div style={{ padding: props.innerPadding || 0 }}>
+        {props.tabs.find((tab) => tab.id === props.curTabID).render()}
+      </div>
     </div>
   );
 }
