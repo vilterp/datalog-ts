592b8d0b2e97d06023e90a9ab09850423b59ff3d
Pete Vilter <pete.vilter@gmail.com>
2024-06-10 00:15:35 -0400

    fix

--- apps/actors/systems/kvSync/gc.ts
+++ apps/actors/systems/kvSync/gc.ts
@@ -1,16 +1,19 @@
 import { filterObj, reversed } from "../../../../util/util";
-import { ClientState } from "./client";
+import { ClientState, TransactionRecord } from "./client";
 import { KVData, VersionedValue } from "./types";
 
 export function garbageCollectTransactions(state: ClientState): ClientState {
+  console.log("===== gc =====");
   const refCounts: { [txnID: string]: number } = {};
   const newData: KVData = {};
+
   // iterate through each key, refcounting transactions
   for (const key in state.data) {
+    console.log("getting refcounts for key", key);
     const vvs = state.data[key];
+    const outputVVs: VersionedValue[] = [];
     for (const vv of reversed(vvs)) {
       const txn = state.transactions[vv.transactionID];
-      const outputVVs: VersionedValue[] = [];
       let foundCommittedTxn = false;
       switch (txn.state.type) {
         case "Pending":
@@ -30,18 +33,34 @@ export function garbageCollectTransactions(state: ClientState): ClientState {
           outputVVs.push(vv);
           break;
       }
-      newData[key] = reversed(outputVVs);
     }
+    newData[key] = reversed(outputVVs);
   }
 
-  const newTransactions = filterObj(
-    state.transactions,
-    (txnID) => refCounts[txnID] > 0
-  );
+  console.log("refCounts", refCounts);
+
+  // const newTransactions = filterObj(
+  //   state.transactions,
+  //   (txnID) => refCounts[txnID] > 0
+  // );
+  const newTransactions: { [txnID: string]: TransactionRecord } = {};
+  for (const txnID in state.transactions) {
+    if (refCounts[txnID] > 0) {
+      newTransactions[txnID] = state.transactions[txnID];
+    } else {
+      console.log(
+        "GC: deleting transaction",
+        txnID,
+        "with status",
+        state.transactions[txnID].state.type
+      );
+    }
+  }
 
   return {
     ...state,
     data: newData,
     transactions: newTransactions,
   };
+  // return state;
 }
