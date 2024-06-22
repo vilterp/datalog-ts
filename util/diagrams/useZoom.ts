3d19baf897a05cc2f228ac42c1c6f3b3a9630246
Pete Vilter <pete.vilter@gmail.com>
2024-06-22 14:40:17 -0400

    start worldToView and viewToWorld

--- util/diagrams/useZoom.ts
+++ util/diagrams/useZoom.ts
@@ -1,4 +1,5 @@
 import { Ref, useEffect, useReducer, useRef } from "react";
+import { linearInterpolate } from "./util";
 
 export type ZoomState = {
   focusPos: number;
@@ -50,12 +51,6 @@ export function useZoom(): [Ref<SVGSVGElement>, ZoomState] {
   return [svgRef, zoomState];
 }
 
-const SENSITIVITY = 0.001;
-
-function zoomPercentage(zoomAbs: number): number {
-  return (1 / (1 + Math.exp(SENSITIVITY * zoomAbs))) * 2;
-}
-
 type ZoomEvt = {
   pos: number;
   delta: number;
@@ -66,4 +61,29 @@ function reducer(state: ZoomStateInternal, evt: ZoomEvt): ZoomStateInternal {
     focusPos: evt.pos, // need to map from scroll space back to world space
     zoomAbs: Math.max(0, state.zoomAbs - evt.delta),
   };
+}
+
+// computations
+
+const SENSITIVITY = 0.001;
+
+function zoomPercentage(zoomAbs: number): number {
+  return (1 / (1 + Math.exp(SENSITIVITY * zoomAbs))) * 2;
+}
+
+// where 'world space' is [0, 1]
+export function worldToView(
+  state: ZoomState,
+  viewWidth: number,
+  point: number
+): number {
+  return linearInterpolate([0, 1], [0, viewWidth], point);
+}
+
+export function viewToWorld(
+  state: ZoomState,
+  viewWidth: number,
+  point: number
+): number {
+  return linearInterpolate([0, viewWidth], [0, 1], point);
 }
