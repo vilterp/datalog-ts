41937ee56f853af5aec93b7657d25c99b27232ad
Pete Vilter <pete.vilter@gmail.com>
2024-10-06 15:56:39 -0400

    fix

--- apps/actors/systems/kvSync/index.ts
+++ apps/actors/systems/kvSync/index.ts
@@ -14,7 +14,7 @@ import { initialServerState, ServerState, updateServer } from "./server";
 import { MsgToClient, MsgToServer, TSMutationDefns } from "./types";
 import { EXAMPLES } from "./examples";
 import { KVApp } from "./examples/types";
-import { hashString, lastItem } from "../../../../util/util";
+import { hashString } from "../../../../util/util";
 
 export const KVSYNC_SYSTEMS = Object.values(EXAMPLES).map(makeActorSystem);
 
